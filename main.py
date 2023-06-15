b1f6df7e2d8149ac509b1592d555f7015eb32070
Doober <doober@Doobers-MacBook-Pro.local>
2023-06-15 12:24:43 -0400

    Added basic error handling for websocket connections

--- main.py
+++ main.py
@@ -58,34 +58,38 @@ async def account_stats_feed(symbol: str):
 
     websocket_stream = 'wss://stream.bybit.com/v5/private'
 
-    async with websockets.connect(websocket_stream) as websocket:
-        
-        req, topics = PrivateWs(Config().api_key(), Config().api_key()).multi_stream_request(['Position', 'Order'])
+    async for websocket in websockets.connect(websocket_stream):
 
-        _auth = await websocket.send(PrivateWs(Config().api_key(), Config().api_key()).auth())
-        print('Successfully authenticated to account private feed...')
+        try: 
+            req, topics = PrivateWs(Config().api_key(), Config().api_key()).multi_stream_request(['Position', 'Order'])
 
-        _sub = await websocket.send(req)
-        print('Subscribed to {} feeds...'.format(topics))
+            _auth = await websocket.send(PrivateWs(Config().api_key(), Config().api_key()).auth())
+            print('Successfully authenticated to account private feed...')
 
-        while True:
+            _sub = await websocket.send(req)
+            print('Subscribed to {} feeds...'.format(topics))
 
-            recv = json.loads(await websocket.recv())
-            
-            if 'success' in recv:
-                pass
+            while True:
 
-            elif recv['data'][0]['symbol'] == symbol:
-
-                data = recv['data']
-
-                if recv['topic'] == topics[0]:
-                    # Generating inventory delta from position feed updates \
-                    # Add inventory control here \
-                    inventory_delta = Inventory(data[-1]).calculate_delta(account_size)
-
-                if recv['topic'] == topics[1]:
+                recv = json.loads(await websocket.recv())
+                
+                if 'success' in recv:
                     pass
+
+                elif recv['data'][0]['symbol'] == symbol:
+
+                    data = recv['data']
+
+                    if recv['topic'] == topics[0]:
+                        # Generating inventory delta from position feed updates \
+                        # Add inventory control here \
+                        inventory_delta = Inventory(data[-1]).calculate_delta(account_size)
+
+                    if recv['topic'] == topics[1]:
+                        pass
+
+        except websockets.ConnectionClosed:
+            continue
 
 
 # Function that updates volatility every few seconds using klines \
@@ -101,42 +105,47 @@ async def volatility_feed(symbol: str, interval: str, lookback: int, norm_passes
 
     websocket_stream = 'wss://stream.bybit.com/v5/public/linear'
 
-    async with websockets.connect(websocket_stream) as websocket:
-        req, topics = PublicWs(symbol).multi_stream_request(['Kline'], interval=interval)
-        _sub = await websocket.send(req)
-        print('Subscribed to {} feeds...'.format(topics))
+    async for websocket in websockets.connect(websocket_stream):
 
-        while True:
+        try:
+            req, topics = PublicWs(symbol).multi_stream_request(['Kline'], interval=interval)
+            _sub = await websocket.send(req)
+            print('Subscribed to {} feeds...'.format(topics))
 
-            recv = json.loads(await websocket.recv())
-            
-            if 'success' in recv:
-                pass
-            
-            elif recv['topic'] == topics[0]:
-                # This is processing klines updates \
-                # Used to attain close values and calculate volatility \
-                # If candle close, shift array -1 and add new value \
-                # Otherwise, update the most recent candle close value \
+            while True:
 
-                klines_data = recv['data']
+                recv = json.loads(await websocket.recv())
+                
+                if 'success' in recv:
+                    pass
+                
+                elif recv['topic'] == topics[0]:
+                    # This is processing klines updates \
+                    # Used to attain close values and calculate volatility \
+                    # If candle close, shift array -1 and add new value \
+                    # Otherwise, update the most recent candle close value \
 
-                for candle in klines_data:
-                    new_hl = np.array([float(candle['high']), float(candle['low'])], dtype=np.float64)
+                    klines_data = recv['data']
 
-                    if candle['confirm'] == True:
-                        klines_hl = np.append(arr=klines_hl[1:], values=new_hl.reshape(1, 2), axis=0)
+                    for candle in klines_data:
+                        new_hl = np.array([float(candle['high']), float(candle['low'])], dtype=np.float64)
 
-                    else:
-                        klines_hl[-1] = new_hl
-                    
-                    volatility_value = simple_range(
-                        arr_in = klines_hl, 
-                        lookback = lookback, 
-                        norm_passes = norm_passes
-                    )
+                        if candle['confirm'] == True:
+                            klines_hl = np.append(arr=klines_hl[1:], values=new_hl.reshape(1, 2), axis=0)
 
-                    volatility_value += volatility_offset
+                        else:
+                            klines_hl[-1] = new_hl
+                        
+                        volatility_value = simple_range(
+                            arr_in = klines_hl, 
+                            lookback = lookback, 
+                            norm_passes = norm_passes
+                        )
+
+                        volatility_value += volatility_offset
+
+        except websockets.ConnectionClosed:
+            continue
                 
 
 # Function that updates mark price and BBA value after every ticker update \
@@ -163,67 +172,71 @@ async def main(symbol: str):
 
     websocket_stream = 'wss://stream.bybit.com/v5/public/linear'
 
-    async with websockets.connect(websocket_stream) as websocket:
-        req, topics = PublicWs(symbol).multi_stream_request(['Ticker'])
-        _sub = await websocket.send(req)
-        print('Subscribed to {} feeds...'.format(topics))
+    async for websocket in websockets.connect(websocket_stream):
 
-        # Start all async futures, updating all global vars in the background \
-        _refreshparams = asyncio.create_task(refresh_parameters(param_dir, 0.1))
-        _accountfeed = asyncio.create_task(account_stats_feed(symbol))
-        _volatilityfeed = asyncio.create_task(volatility_feed(symbol, '5', 5, 0))
-        _sleep = await asyncio.sleep(1)   # Small timeout to let feeds warm up with data 
+        try:
+            req, topics = PublicWs(symbol).multi_stream_request(['Ticker'])
+            _sub = await websocket.send(req)
+            print('Subscribed to {} feeds...'.format(topics))
 
-        while True:
+            # Start all async futures, updating all global vars in the background \
+            _refreshparams = asyncio.create_task(refresh_parameters(param_dir, 0.1))
+            _accountfeed = asyncio.create_task(account_stats_feed(symbol))
+            _volatilityfeed = asyncio.create_task(volatility_feed(symbol, '5', 5, 0))
+            _sleep = await asyncio.sleep(1)   # Small timeout to let feeds warm up with data 
 
-            recv = json.loads(await websocket.recv())
-            
-            if 'success' in recv:
-                pass
-            
-            elif recv['topic'] == topics[0]:
-                # This is processing ticker updates \
-                # Used to attain spread and mark price \
+            while True:
+
+                recv = json.loads(await websocket.recv())
                 
-                ticker_data = recv['data']
-
-                if 'bid1Price' in ticker_data:
-                    best_bid_price = float(ticker_data['bid1Price'])
-
-                if 'ask1Price' in ticker_data:
-                    best_ask_price = float(ticker_data['ask1Price'])
-
-                if 'markPrice' in ticker_data:
-                    new_mark_price = round_step_size(float(ticker_data['markPrice']), tick_size)
+                if 'success' in recv:
+                    pass
+                
+                elif recv['topic'] == topics[0]:
+                    # This is processing ticker updates \
+                    # Used to attain spread and mark price \
                     
-                    # Run the strategy and refresh quotes if the mark price has changed \
-                    if new_mark_price == mark_price:
-                        pass
+                    ticker_data = recv['data']
 
-                    else:
-                        mark_price = new_mark_price
+                    if 'bid1Price' in ticker_data:
+                        best_bid_price = float(ticker_data['bid1Price'])
 
-                        _cancel = await Order(Config().api_key(), Config().api_secret(), symbol).cancel_all()
+                    if 'ask1Price' in ticker_data:
+                        best_ask_price = float(ticker_data['ask1Price'])
 
-                        _orders = Strategy(
-                                tick_size=tick_size,
-                                lot_size=lot_size,
-                                target_spread=target_spread,
-                                minimum_order_size=minimum_order_size,
-                                maximum_order_size=maximum_order_size,
-                                quote_offset=quote_offset,
-                                size_offset=size_offset
-                            ).market_maker(
-                                mark_price=mark_price,
-                                volatility=volatility_value,
-                                bba=(best_bid_price, best_ask_price),
-                                inventory_delta=inventory_delta,
-                                inventory_extreme=inventory_extreme
-                        )
+                    if 'markPrice' in ticker_data:
+                        new_mark_price = round_step_size(float(ticker_data['markPrice']), tick_size)
                         
-                        _execution = await Order(Config().api_key(), Config().api_secret(), symbol).batch_orders(_orders)
-                        
+                        # Run the strategy and refresh quotes if the mark price has changed \
+                        if new_mark_price == mark_price:
+                            pass
 
+                        else:
+                            mark_price = new_mark_price
+
+                            _cancel = await Order(Config().api_key(), Config().api_secret(), symbol).cancel_all()
+
+                            _orders = Strategy(
+                                    tick_size=tick_size,
+                                    lot_size=lot_size,
+                                    target_spread=target_spread,
+                                    minimum_order_size=minimum_order_size,
+                                    maximum_order_size=maximum_order_size,
+                                    quote_offset=quote_offset,
+                                    size_offset=size_offset
+                                ).market_maker(
+                                    mark_price=mark_price,
+                                    volatility=volatility_value,
+                                    bba=(best_bid_price, best_ask_price),
+                                    inventory_delta=inventory_delta,
+                                    inventory_extreme=inventory_extreme
+                            )
+                            
+                            _execution = await Order(Config().api_key(), Config().api_secret(), symbol).batch_orders(_orders)
+
+        except websockets.ConnectionClosed:
+            continue
+                        
 
 if __name__ == "__main__":
 
