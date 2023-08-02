f53ff1f993098f0402bf9ee7e9c8dc8a60a916c7
beatzxbt <mpatel.vishva@gmail.com>
2023-08-02 12:51:46 +0300

    .

--- src/indicators/bbw.py
+++ src/indicators/bbw.py
@@ -1,15 +1,23 @@
 import numpy as np
-from numba import njit
+from numba import njit, prange
 from src.indicators.ema import ema
 
-@njit(nogil=True)
+
+@njit(parallel=True, nogil=True)
 def bbw(arr_in: np.array, length: int, std: int) -> np.array:
     """
     Hyper-fast Bollinger Band Width implementation \n
     Be careful with the data type in the array! 
     """
-    basis = ema(arr_in, length)
-    dev = std * np.std(arr_in[-length:])
+
+    n = len(arr_in)
+    close = np.empty(n, dtype=np.float64)
+
+    for i in prange(n):
+        close[i] = arr_in[i][4]
+
+    basis = ema(close, length)
+    dev = std * np.std(close[-length:])
     upper = basis[-1] + dev
     lower = basis[-1] - dev
     bbw = np.sqrt(upper-lower)
--- src/indicators/ema.py
+++ src/indicators/ema.py
@@ -1,14 +1,15 @@
 import numpy as np
-from numba import njit, float64, int64
+from numba import njit
 
-@njit((float64[:], int64), nogil=True)
+
+@njit(nogil=True)
 def ema(arr_in: np.array, window: int) -> np.array:
     """
     Hyper-fast EMA implementation \n
     Be careful with the data type in the array! 
     """
     n = arr_in.shape[0]
-    ewma = np.empty(n, dtype=float64)
+    ewma = np.empty(n, dtype=float)
     alpha = 2 / float(window + 1)
     w = 1
     ewma_old = arr_in[0]
