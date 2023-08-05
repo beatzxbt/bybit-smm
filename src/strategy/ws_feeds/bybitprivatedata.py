0600438917e773e2ee638284f2586e3e053a3018
beatzxbt <mpatel.vishva@gmail.com>
2023-08-05 17:20:30 +0300

    added auth retry logic

--- src/strategy/ws_feeds/bybitprivatedata.py
+++ src/strategy/ws_feeds/bybitprivatedata.py
@@ -1,5 +1,6 @@
 import websockets
 import orjson
+import asyncio
 
 from src.bybit.websockets.endpoints import WsStreamLinks
 from src.bybit.websockets.private import PrivateWs
@@ -23,7 +24,46 @@ class BybitPrivateData:
         self.private_ws = PrivateWs(self.api_key, self.api_secret)
 
 
-    async def privatefeed(self):
+    async def authenticate_feed(self, websocket):
+
+        # Send first auth attempt and wait for response \
+        await websocket.send(self.private_ws.auth())
+        auth_recv = orjson.loads(await websocket.recv())
+        
+        # If successful, continue... \
+        if auth_recv['success'] is True:
+            return 
+
+        # If it failed, run a retry loop 
+        for i in range(2):
+            
+            # Exponential backoff for retries \
+            await asyncio.sleep(1 + i**2)
+
+            try:
+                # Reinitialize the class to reset the timer, then send \
+                self.private_ws = PrivateWs(self.api_key, self.api_secret)
+                await websocket.send(self.private_ws.auth())
+                auth_recv = orjson.loads(await websocket.recv())    
+
+                # If successful, continue... \
+                if auth_recv['success'] is True:
+                    return 
+
+                # Trigger exception to re-run loop \
+                else: 
+                    raise Exception
+
+            # Catch exception and move onto next iteration \
+            except Exception as e:
+                continue
+        
+        # Raise final exception to halt feed \
+        print('Authentication for private feed failed...')
+        raise Exception
+
+
+    async def private_feed(self):
         
         req, topics = self.private_ws.multi_stream_request(['Position', 'Execution'])
 
@@ -32,24 +72,24 @@ class BybitPrivateData:
         async for websocket in websockets.connect(WsStreamLinks.combined_private_stream()):
             
             try:
-                await websocket.send(self.private_ws.auth())
+                await self.authenticate_feed(websocket)
                 await websocket.send(req)
 
                 while True:
                     
                     recv = orjson.loads(await websocket.recv())
 
+                    # Skip the streams subscription message \
                     if 'success' in recv:
-                        pass
-                    
-                    else:
-                        data = recv['data']
+                        continue
 
-                        if recv['topic'] == topics[0]:
-                            BybitPositionHandler(self.ss, data).process()
+                    data = recv['data']
 
-                        if recv['topic'] == topics[1]:
-                            BybitExecutionHandler(self.ss, data).process()
+                    if recv['topic'] == topics[0]:
+                        BybitPositionHandler(self.ss, data).process()
+
+                    if recv['topic'] == topics[1]:
+                        BybitExecutionHandler(self.ss, data).process()
                         
 
             except websockets.ConnectionClosed:
@@ -61,4 +101,4 @@ class BybitPrivateData:
 
 
     async def start_feed(self):
-        await self.privatefeed()
+        await self.private_feed()
