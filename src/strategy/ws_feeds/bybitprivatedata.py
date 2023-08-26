ef3de6af210c88e5061cf250d84b2cd4986010f9
beatzxbt <mpatel.vishva@gmail.com>
2023-08-26 11:09:11 +0300

    syncing for position & orders
    - now syncs all orders and current position every 500ms

--- src/strategy/ws_feeds/bybitprivatedata.py
+++ src/strategy/ws_feeds/bybitprivatedata.py
@@ -2,20 +2,25 @@ import websockets
 import orjson
 import asyncio
 
-from src.bybit.websockets.endpoints import WsStreamLinks
-from src.bybit.websockets.private import PrivateWs
 from src.utils.misc import Misc
 
-from src.bybit.websockets.handlers.execution import BybitExecutionHandler
-from src.bybit.websockets.handlers.position import BybitPositionHandler
+from src.exchanges.bybit.websockets.endpoints import WsStreamLinks
+from src.exchanges.bybit.websockets.private import PrivateWs
+from src.exchanges.bybit.websockets.handlers.execution import BybitExecutionHandler
+from src.exchanges.bybit.websockets.handlers.position import BybitPositionHandler
+from src.exchanges.bybit.websockets.handlers.order import BybitOrderHandler
 
+from src.exchanges.bybit.get.private import BybitPrivateClient
+
+
+from src.sharedstate import SharedState
 
 
 class BybitPrivateData:
 
 
-    def __init__(self, sharedstate) -> None:
-        
+    def __init__(self, sharedstate: SharedState) -> None:
+
         self.ss = sharedstate
         self.api_key = self.ss.api_key
         self.api_secret = self.ss.api_secret
@@ -24,73 +29,79 @@ class BybitPrivateData:
         self.private_ws = PrivateWs(self.api_key, self.api_secret)
 
 
-    async def authenticate_feed(self, websocket):
+    async def open_orders_sync(self):
+        """
+        Will sync the open orders dict every 0.5s
+        """
 
-        # Send first auth attempt and wait for response \
-        await websocket.send(self.private_ws.auth())
-        auth_recv = orjson.loads(await websocket.recv())
+        while True:
+
+            recv = await BybitPrivateClient(self.ss).open_orders()
+            data = recv['result']['list']
+
+            curr_orders = {}
+
+            for open_order in data: 
+
+                id = open_order['orderId']
+                price = float(open_order['price'])
+                qty = float(open_order['qty'])
+                side = open_order['side']
+                
+                curr_orders[id] = {'price': price, 'qty': qty, 'side': side}
+
+            self.ss.current_orders = curr_orders
+
+            # Sleep for 0.5s \
+            await asyncio.sleep(0.5)
+
+
+    async def current_position_sync(self):
+        """
+        Will sync the open orders dict every 0.5s
+        """
+
+        while True:
+
+            recv = await BybitPrivateClient(self.ss).current_position()
+            data = recv['result']['list']
+
+            BybitPositionHandler(self.ss, data).process()
+
+            # Sleep for 0.5s \
+            await asyncio.sleep(0.5)
+
+
+    async def privatefeed(self):
         
-        # If successful, continue... \
-        if auth_recv['success'] is True:
-            return 
-
-        # If it failed, run a retry loop 
-        for i in range(2):
-            
-            # Exponential backoff for retries \
-            await asyncio.sleep(1 + i**2)
-
-            try:
-                # Reinitialize the class to reset the timer, then send \
-                self.private_ws = PrivateWs(self.api_key, self.api_secret)
-                await websocket.send(self.private_ws.auth())
-                auth_recv = orjson.loads(await websocket.recv())    
-
-                # If successful, continue... \
-                if auth_recv['success'] is True:
-                    return 
-
-                # Trigger exception to re-run loop \
-                else: 
-                    raise Exception
-
-            # Catch exception and move onto next iteration \
-            except Exception as e:
-                continue
-        
-        # Raise final exception to halt feed \
-        print('Authentication for private feed failed...')
-        raise Exception
-
-
-    async def private_feed(self):
-        
-        req, topics = self.private_ws.multi_stream_request(['Position', 'Execution'])
+        req, topics = self.private_ws.multi_stream_request(['Position', 'Execution', 'Order'])
 
         print(f"{Misc.current_datetime()}: Subscribed to BYBIT {topics} feeds...")
         
         async for websocket in websockets.connect(WsStreamLinks.combined_private_stream()):
             
             try:
-                await self.authenticate_feed(websocket)
+                await websocket.send(self.private_ws.auth())
                 await websocket.send(req)
 
                 while True:
                     
                     recv = orjson.loads(await websocket.recv())
 
-                    # Skip the streams subscription message \
                     if 'success' in recv:
-                        continue
+                        pass
+                    
+                    else:
+                        data = recv['data']
 
-                    data = recv['data']
+                        if recv['topic'] == topics[0]:
+                            BybitPositionHandler(self.ss, data).process()
 
-                    if recv['topic'] == topics[0]:
-                        BybitPositionHandler(self.ss, data).process()
-
-                    if recv['topic'] == topics[1]:
-                        BybitExecutionHandler(self.ss, data).process()
+                        if recv['topic'] == topics[1]:
+                            BybitExecutionHandler(self.ss, data).process()
                         
+                        if recv['topic'] == topics[2]:
+                            BybitOrderHandler(self.ss, data).process()
 
             except websockets.ConnectionClosed:
                 continue
@@ -101,4 +112,11 @@ class BybitPrivateData:
 
 
     async def start_feed(self):
-        await self.private_feed()
+
+        tasks = []
+
+        tasks.append(asyncio.create_task(self.open_orders_sync()))
+        tasks.append(asyncio.create_task(self.current_position_sync()))
+        tasks.append(asyncio.create_task(self.privatefeed()))
+
+        await asyncio.gather(*tasks)
