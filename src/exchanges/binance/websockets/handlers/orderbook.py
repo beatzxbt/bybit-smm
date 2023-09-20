15cd4ff1eaf30452c30e809d00983873fd873f34
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2023-09-20 10:14:30 +0100

    fixed bug causing ob to become unordered
    now resorts book on snapshot and process updates, inline with bybit ob methods

--- src/exchanges/binance/websockets/handlers/orderbook.py
+++ src/exchanges/binance/websockets/handlers/orderbook.py
@@ -9,16 +9,17 @@ class OrderBookBinance(BaseOrderBook):
     def process_snapshot(self, snapshot: dict):
         self.asks = np.array(snapshot["asks"], dtype=float)
         self.bids = np.array(snapshot["bids"], dtype=float)
+        self.sort_book()
 
 
     def process(self, recv: dict):
         data = recv["data"]
-
         asks = np.array(data["a"], dtype=float)
         bids = np.array(data["b"], dtype=float)
 
         self.asks = self.update_book(self.asks, asks)
         self.bids = self.update_book(self.bids, bids)
+        self.sort_book()
 
 
 
