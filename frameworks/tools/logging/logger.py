b064e1144819b0014141b08a75c69834ce86f94d
Beta <105949605+LevBeta@users.noreply.github.com>
2023-11-03 00:30:55 +0000

    Fix logger

--- frameworks/tools/logging/logger.py
+++ frameworks/tools/logging/logger.py
@@ -9,25 +9,25 @@ class Logger:
         pass
 
     
-    def success(msg: str) -> None:
+    def success(self, msg: str) -> None:
         print(f"{now()} | SUCCESS | {msg}")
 
 
-    def info(msg: str) -> None:
+    def info(self, msg: str) -> None:
         print(f"{now()} | INFO | {msg}")
 
 
-    def debug(msg: str) -> None:
+    def debug(self, msg: str) -> None:
         print(f"{now()} | DEBUG | {msg}")
 
     
-    def warning(msg: str) -> None:
+    def warning(self, msg: str) -> None:
         print(f"{now()} | WARNING | {msg}")
 
 
-    def error(msg: str) -> None:
+    def error(self, msg: str) -> None:
         print(f"{now()} | ERROR | {msg}")
 
 
-    def critical(msg: str) -> None:
+    def critical(self, msg: str) -> None:
         print(f"{now()} | CRITICAL | {msg}")
