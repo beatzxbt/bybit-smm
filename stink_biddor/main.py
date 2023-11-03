c5c180944ca82154fe7c20bbf24ef9e38cc455f9
beatzxbt <mpatel.vishva@gmail.com>
2023-11-03 11:27:08 +0000

    fixing import errors
    -removed boilerplate code
    
    -added more __init__'s
    
    -now running as a module (python3 -m ...)

--- stink_biddor/main.py
+++ stink_biddor/main.py
@@ -1,20 +1,9 @@
-import sys
-import os
-
-# Get the absolute path to the project root directory
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-# Add the project root directory to sys.path
-sys.path.insert(0, project_root)
-
 
 import asyncio
-
-from strategy.core import Strategy
-from settings import StrategyParameters
+from stink_biddor.strategy.core import Strategy
+from stink_biddor.settings import StrategyParameters
 from frameworks.sharedstate.market import MarketDataSharedState
 from frameworks.sharedstate.private import PrivateDataSharedState
-
 
 
 async def main(configuration_directory: str, parameter_directory: str):
