d24bbc47e5c730f0a43a6c4d4c234223252f0768
Beta <105949605+LevBeta@users.noreply.github.com>
2023-11-06 20:35:13 +0000

    Add clear() function to BaseOrderBook

--- frameworks/exchange/common/localorderbook.py
+++ frameworks/exchange/common/localorderbook.py
@@ -14,6 +14,9 @@ class BaseOrderBook:
         self.asks = self.asks[self.asks[:, 0].argsort()]
         self.bids = self.bids[self.bids[:, 0].argsort()[::-1]]
 
+    def clear_book(self):
+        #ONLY TO BE USED WHEN A CONNECTION TO A WS IS DROPPED
+        self.__init__()
 
     def update_book(self, asks_or_bids, data):
         for price, qty in data:
