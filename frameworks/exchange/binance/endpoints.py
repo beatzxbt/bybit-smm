e37ce4c567fe08ffead8b07b83763a531a5b8823
beatzxbt <mpatel.vishva@gmail.com>
2023-11-22 22:39:22 +0000

    binance private client
    -added progress on binance private client
    
    still required:
    -depreciate batch endpoints
    -endpoints need to be changed in main order class
    -tests

--- frameworks/exchange/binance/endpoints.py
+++ frameworks/exchange/binance/endpoints.py
@@ -3,7 +3,20 @@ from dataclasses import dataclass
 
 
 @dataclass
+class BaseEndpoints:
+    MAINNET1 = "https://fapi.binance.com"
+
+
+@dataclass
 class WsStreamLinks:
     SPOT_PUBLIC_STREAM = "wss://stream.binance.com:9443"
     FUTURES_PUBLIC_STREAM = "wss://fstream.binance.com"
+
+
+@dataclass
+class PrivatePostLinks:
+    CREATE_ORDER = "/fapi/v1/order"
+    AMEND_ORDER = "/fapi/v1/order"
+    CANCEL_SINGLE = "/fapi/v1/order"
+    CANCEL_ALL = "/fapi/v1/allOpenOrders"
     
