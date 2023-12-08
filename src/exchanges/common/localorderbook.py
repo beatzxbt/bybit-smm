723bf5edcc9c13d1960872031f5a1f7c41a2d205
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2023-12-08 03:03:47 +0000

    fix: ob size mem leak
    added a (1000) max limit for orderbook levels on either side, preventing mem leak
    
    unlikely that one will require more than that, but can increase the limit as required

--- src/exchanges/common/localorderbook.py
+++ src/exchanges/common/localorderbook.py
@@ -4,19 +4,15 @@ import numpy as np
 
 class BaseOrderBook:
 
-
     def __init__(self):
         self.asks = np.empty((0, 2), float)
         self.bids = np.empty((0, 2), float)
 
-
     def sort_book(self):
-        self.asks = self.asks[self.asks[:, 0].argsort()]
-        self.bids = self.bids[self.bids[:, 0].argsort()[::-1]]
-
+        self.asks = self.asks[self.asks[:, 0].argsort()][:1000]
+        self.bids = self.bids[self.bids[:, 0].argsort()[::-1]][:1000]
 
     def update_book(self, asks_or_bids, data):
-
         for price, qty in data:
             asks_or_bids = asks_or_bids[asks_or_bids[:, 0] != price]
 
