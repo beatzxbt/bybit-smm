fc2e86853e6db543fef92a8e270905720971a398
beatzxbt <mpatel.vishva@gmail.com>
2024-01-06 14:09:07 +0000

    add: depreciate batch endpoints

--- frameworks/exchange/binance/post/order.py
+++ frameworks/exchange/binance/post/order.py
@@ -7,117 +7,74 @@ from frameworks.exchange.binance.endpoints import PrivatePostLinks
 from frameworks.exchange.binance.post.types import OrderTypesFutures
 from frameworks.sharedstate.private import PrivateDataSharedState
 
+from typing import List, Dict, Tuple
+
 
 class BinanceOrder:
     """ 
-    {order}: Tuple of struct (side: string, price: float, qty: float)
+    {order}: Tuple of struct (side: float, price: float, qty: float)
     """
 
-    def __init__(self, sharedstate: PrivateDataSharedState, symbol: str) -> None:
-        self.pdss = sharedstate
+    def __init__(
+        self, 
+        sharedstate: PrivateDataSharedState, 
+        symbol: str
+    ) -> None:
+
+        self.client = BinancePrivatePostClient(sharedstate)
         self.futures = OrderTypesFutures(symbol)
-        self.client = BinancePrivatePostClient(self.pdss)
         self.endpoints = PrivatePostLinks
         self.session = aiohttp.ClientSession()
 
 
-    def _order_to_str(self, order) -> tuple[str, ...]:
+    def _map_to_str(self, order: Tuple) -> Tuple[str, ...]:
         return tuple(map(str, order))
 
-    
-    async def _submit(self, endpoint: str, payload: dict) -> dict | None:
-        async with self.session:
-            return await self.client.submit(self.session, endpoint, payload)
+
+    def _set_order_side(self, side: int | float) -> str:
+        return "BUY" if side == 1 else "SELL"
 
 
-    async def _sessionless_submit(self, endpoint: str, payload: dict) -> dict | None:
+    async def _submit(self, endpoint: str, payload: Dict) -> Dict | None:
         return await self.client.submit(self.session, endpoint, payload)
 
 
-    async def order_market(self, order: tuple, tp: float=None) -> dict | None:
-        endpoint = self.endpoints.CREATE_ORDER
-        str_order = self._order_to_str(order)
+    async def market(self, order: Tuple, tp: float=None) -> Dict | None:
+        endpoint = self.endpoints.CREATE
+        order[0] = self._set_order_side(order[0])
+        str_order = self._map_to_str(order)
         payload = self.futures.market(str_order, tp)
 
         return await self._submit(endpoint, payload)
 
 
-    async def order_limit(self, order: tuple, tp: float=None) -> dict | None:
-        endpoint = self.endpoints.CREATE_ORDER
-        str_order = self._order_to_str(order)
+    async def limit(self, order: Tuple, tp: float=None) -> Dict | None:
+        endpoint = self.endpoints.CREATE
+        order[0] = self._set_order_side(order[0])
+        str_order = self._map_to_str(order)
         payload = self.futures.limit(str_order, tp)
 
         return await self._submit(endpoint, payload)
 
 
-    async def order_batch(self, orders: list) -> dict | None:
-        batch_endpoint = self.endpoints.CREATE_BATCH
-        tasks = []
-        n = len(orders)
-
-        # Split the orders into chunks of 10
-        for i in range(0, n, 10):
-            str_orders = [self._order_to_str(order) for order in orders[i:i+10]]
-            batch = [self.futures.limit(order) for order in str_orders]
-            batch_payload = {"category": "linear", "request": batch}
-            task = self._sessionless_submit(batch_endpoint, batch_payload)
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)
-        await self.session.close()
-
-
-    async def amend(self, order: tuple, tp: float=None) -> dict | None:
-        endpoint = self.endpoints.AMEND_ORDER
-        str_order = self._order_to_str(order)
+    async def amend(self, order: Tuple, tp: float=None) -> Dict | None:
+        endpoint = self.endpoints.CREATE
+        order[0] = self._set_order_side(order[0])
+        str_order = self._map_to_str(order)
         payload = self.futures.amend(str_order, tp)
 
         return await self._submit(endpoint, payload)
 
 
-    async def amend_batch(self, orders: list) -> dict | None:
-        batch_endpoint = self.endpoints.AMEND_BATCH
-        tasks = []
-        n = len(orders)
-
-        # Split the orders into chunks of 10
-        for i in range(0, n, 10):
-            str_orders = [self._order_to_str(order) for order in orders[i:i+10]]
-            batch = [self.futures.amend(order) for order in str_orders]
-            batch_payload = {"category": "linear", "request": batch}
-            task = self._sessionless_submit(batch_endpoint, batch_payload)
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)
-        await self.session.close()
-
-
-    async def cancel(self, orderId: str) -> dict | None:
-        endpoint = self.endpoints.CANCEL_SINGLE
+    async def cancel(self, orderId: str) -> Dict | None:
+        endpoint = self.endpoints.CREATE
         payload = self.futures.cancel(orderId)
 
         return await self._submit(endpoint, payload)
 
 
-    async def cancel_batch(self, orderIds: list) -> dict | None:
-        batch_endpoint = self.endpoints.CANCEL_BATCH
-        tasks = []
-        n = len(orderIds)
-
-        # Split the orderIds into chunks of 10
-        for i in range(0, n, 10):
-            batch = [self.futures.cancel(order) for order in orderIds[i:i+10]]
-            batch_payload = {"category": "linear", "request": batch}
-            task = self._sessionless_submit(batch_endpoint, batch_payload)
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)
-        await self.session.close()
-
-
-    async def cancel_all(self) -> dict | None:
+    async def cancel_all(self) -> Dict | None:
         endpoint = self.endpoints.CANCEL_ALL
         payload = self.futures.cancel_all()
-        self._update_rate_limits("cancel_all", 1)
 
         return await self._submit(endpoint, payload)
