538d7b23ce205f976a78cbb25330c5d2deaf284e
beatzxbt <mpatel.vishva@gmail.com>
2024-01-11 23:14:56 +0000

    add: using bba method

--- smm/strategy/features/bba_imbalance.py
+++ smm/strategy/features/bba_imbalance.py
@@ -1,11 +1,7 @@
-
+from numpy.typing import NDArray
 from numba import njit, float64
 
-
-@njit(float64(float64, float64))
-def bba_imbalance(bid: float, ask: float) -> float:
-    """
-    Imbalance between bid and ask quantities
-    """
-
-    return ((bid/(ask + bid)) - 0.5) * 2
+@njit(float64(float64[:]), cache=True)
+def bba_imbalance(bba: NDArray) -> float:
+    """Imbalance between bid and ask quantities"""
+    return ((bba[0][1]/(bba[1][1] + bba[0][1])) - 0.5) * 2
