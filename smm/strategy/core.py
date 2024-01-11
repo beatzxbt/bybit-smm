d9814a848f8fce77b31a85e073782b3663b28a7d
beatzxbt <mpatel.vishva@gmail.com>
2024-01-11 22:43:36 +0000

    add: rough dependency injection for quote gen

--- smm/strategy/core.py
+++ smm/strategy/core.py
@@ -1,24 +1,28 @@
 import asyncio
 from frameworks.sharedstate import SharedState
-from smm.strategy.quote_generators.simple import SimpleQuoteGenerator
-from smm.strategy.quote_generators.basic import BasicQuoteGenerator
 from smm.settings import SmmParameters
-from smm.strategy.diff import Diff
+from smm.strategy.oms import OrderManagementSystem
 
 
 class MarketMaker:
 
-    def __init__(self, ss: SharedState) -> None:
+    def __init__(self, ss: SharedState, params: SmmParameters) -> None:
         self.ss = ss
-        self.params  = SmmParameters()
+        self.params = params
         self.logger = self.ss.logging
 
         self.quote_generator = None
+        self.oms = OrderManagementSystem(self.ss)
 
     def _strategy_selector_(self) -> None:
         if self.quote_generator is None:
             if self.params["quote_generator"] == "simple":
+                from smm.strategy.quote_generators.simple import SimpleQuoteGenerator
                 self.quote_generator = SimpleQuoteGenerator(self.ss, self.params["simple"])
+
+            elif self.params["quote_generator"] == "basic":
+                from smm.strategy.quote_generators.basic import BasicQuoteGenerator
+                self.quote_generator = BasicQuoteGenerator(self.ss, self.params["basic"])
 
     async def _loop_(self):
         self.logger.info("Warming up data feeds...")
@@ -28,12 +32,8 @@ class MarketMaker:
         
         while True:
             await asyncio.sleep(0.01 * 100)
-            
-            # Add a switch here to let user swap between strategies (in realtime)
             new_orders = self.quote_generator(self.ss, self.params)
-            
-            await Diff(self.ss).diff(new_orders)
-
+            await self.oms.update(new_orders)
 
     async def run(self):
         await asyncio.gather(
