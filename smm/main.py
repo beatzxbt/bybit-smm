59532aa357c3c0f341496d43eed33d52f21f128c
beatzxbt <mpatel.vishva@gmail.com>
2024-01-11 23:15:15 +0000

    add: critical level exception handler

--- smm/main.py
+++ smm/main.py
@@ -1,15 +1,30 @@
 import asyncio
-from smm.strategy.core import MarketMaker
-from smm.settings import SmmParameters
 from frameworks.sharedstate import SharedState
+from frameworks.tools.logger import Logger, now
+from smm.strategy.marketmaker import MarketMaker
+from smm.settings import SmmParameters
 
 
 async def main():
-    ss = SharedState()
-    params = SmmParameters(ss)
-    ss.load_markets(params.markets)
-    await MarketMaker(ss).run()    
+    try:
+        parameters_directory = ""
+        params = SmmParameters(parameters_directory)
 
+        ss = SharedState()
+        ss.load_markets(params.markets)
+
+        await asyncio.gather(
+            asyncio.to_thread(params.refresh_parameters()),
+            asyncio.to_thread(MarketMaker(ss, params).run())
+        )
+
+    except Exception as e:
+        Logger.critical(f"High level exception occurred, shutting down...")
+        # TODO: Implement shut down routine without sharedstate use
+        raise e
+
+    finally:
+        print(f"It's {now()}, goodnight...")
+        
 if __name__ == "__main__":
     asyncio.run(main())
-
