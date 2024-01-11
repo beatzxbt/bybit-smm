c76d8bed9e40d1af761a425b7e8e4921d576dac3
beatzxbt <mpatel.vishva@gmail.com>
2024-01-11 22:42:39 +0000

    add: cache=True

--- frameworks/tools/rounding.py
+++ frameworks/tools/rounding.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numba import njit, float64
 
-@njit(float64(float64, float64))
+@njit(float64(float64, float64), cache=True)
 def round_step(num: float, step: float) -> float:
     p = int(1/step)
     return np.floor(num*p)/p
