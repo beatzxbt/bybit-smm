46296d4b4b0a14e7165e286f607319dd3f9531a9
beatzxbt <mpatel.vishva@gmail.com>
2024-01-11 22:28:42 +0000

    add: clean funcs + remove error handling

--- frameworks/exchange/base/rest/client.py
+++ frameworks/exchange/base/rest/client.py
@@ -1,107 +1,54 @@
-import json
-import time
-import hashlib
-import hmac
-import aiohttp
 import asyncio
-from typing import Dict, List, Tuple, Optional, Union, _T
+import aiohttp
+import orjson
+from time import time_ns
+from typing import Dict, Union
 from frameworks.sharedstate import SharedState
 from frameworks.exchange.base.rest.ratelimits import RateLimitManager
 
 
 class Client:
 
-    def __init__(self, ss: SharedState, rl: RateLimitManager) -> None:
-        self.ss = ss
+    def __init__(self, rl: RateLimitManager) -> None:
         self.rl = rl
         self.logging = self.ss.logging
         self.session = aiohttp.ClientSession()
         self.recvWindow = "5000"
-        self.timestamp = str(int(time.time()*1000))
+        self.timestamp = str(time_ns()//1_000_000)
 
-    @property
-    def _api_(self) -> Dict:
-        """Pointer to exchange's API info"""
-        return self.ss.private[self.exchange]["API"]
+        self.MAX_RETRIES = 3
 
     def _update_timestamp_(self) -> str:
-        self.timestamp = str(int(time.time()*1000))
+        self.timestamp = str(time_ns()//1_000_000)
 
     def _sign_(self, payload: str) -> Dict:
-        self._update_timestamp_()
-        param_str = "".join([self.timestamp, self._api_["key"], self.recvWindow, payload])
+        raise NotImplementedError("Must be implimented in parent class")
 
-        header = {
-            "X-BAPI-TIMESTAMP": self.timestamp,
-            "X-BAPI-API-KEY": self._api_["key"],
-            "X-BAPI-RECV-WINDOW": self.recvWindow,
-        }
+    def _error_handler_(self, response: Dict) -> Union[dict, None]:
+        raise NotImplementedError("Must be implimented in parent class")
 
-        hash_signature = hmac.new(
-            bytes(self._api_["secret"], "utf-8"), 
-            param_str.encode("utf-8"), 
-            hashlib.sha256
-        )
-
-        header["X-BAPI-SIGN"] = hash_signature.hexdigest()
-
-        return header
-
-    async def _post_(self, endpoint: str, payload: dict):
-        payload_str = json.dumps(payload)
-        self.signed_header = self._sign_(payload_str)
-        full_endpoint = self.base_endpoint + endpoint
-
-        max_retries = 3  
+    async def post(self, endpoint: str, header: str, payload: Dict):
+        payload_to_str = orjson.dumps(payload)
         
-        for attempt in range(max_retries):
+        for attempt in range(1, self.MAX_RETRIES+1):
             try:
-                req = await self.session.request("POST", full_endpoint, headers=self.signed_header, data=payload_str)
-                response = json.loads(await req.text())
+                request = await self.session.request(
+                    method="POST", 
+                    endpoint=endpoint, 
+                    headers=header, 
+                    data=payload_to_str
+                )
+
+                response = orjson.loads(await request.text())
                 self.rl.update(endpoint, response)
-
-                if response["retMsg"] == "OK" or response["retMsg"] == "success":
-                    latency = int(response["time"]) - int(self.timestamp)
-
-                    if latency >= 1000:
-                        self.logging.warning(f"High latency detected: {latency}ms | Endpoint: {endpoint}")
-
-                    ret = {
-                        "return" : response["result"],
-                        "latency": latency
-                    }
-
-                    return ret
-
-                # Error handling
-                else:
-                    code = response["retCode"]
-
-                    if code in self.errors.err_codes.keys():
-                        err_response, msg = self.errors.err_codes[code]
-    
-                        if err_response:
-                            self.logging.error(f"{msg} | Endpoint: {endpoint}")
-                            return None
-
-                        # Retry the request
-                        else:
-                            raise Exception
-                            
-                    # Enter other error handling here
-                    else:            
-                        self.logging.error(f"{msg} | Endpoint: {endpoint}")
-                        return None
-
 
             except Exception as e:
                 # Resign the payload and retry the request after sleeping for 1s
-                if attempt < max_retries - 1:  
+                if attempt < self.MAX_RETRIES:  
                     await asyncio.sleep(attempt)  
                     self.signed_header = self._sign_(payload)
 
                 else:
-                    self.logging.critical(f"Exception: {e} | Endpoint: {endpoint}")
-                    return None
+                    raise e
 
         
