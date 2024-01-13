c2de4df56302700db9c9a38332d336dd7c1a6938
beatzxbt <mpatel.vishva@gmail.com>
2024-01-13 00:59:31 +0000

    add: cleaner docstring

--- smm/strategy/features/log_spread.py
+++ smm/strategy/features/log_spread.py
@@ -1,12 +1,7 @@
-
 import numpy as np
 from numba import njit, float64
 
-
-@njit(float64(float64, float64))
+@njit(float64(float64, float64), cache=True)
 def log_spread(price: float, benchmark: float) -> float:
-    """
-    Log difference between two prices (*100)
-    """
-
+    """Log difference between a price and a benchmark"""
     return np.log(price/benchmark) * 100
