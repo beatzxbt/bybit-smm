3bd81d19066abfa5b64a48d86a0417fb64e20b64
beatzxbt <mpatel.vishva@gmail.com>
2024-01-17 17:23:54 +0000

    del: settings processing

--- smm/settings.py
+++ smm/settings.py
@@ -4,23 +4,8 @@ from frameworks.config.custom_parameters import CustomParameters
 
 class SmmParameters(CustomParameters):
 
-    def __init__(self, param_dir: str) -> None:
-        super().__init__(param_dir)
-    
-    
-    def load_parameters(self, settings: Dict):
+    def process_parameters(self, settings: Dict) -> None:
+        self.primary_exchange = str(settings["primary_exchange"])
         self.pairs = list(settings["symbols"])
-
-        self.bollinger_band_length = int(settings["bollinger_band_length"])
-        self.bollinger_band_std = int(settings["bollinger_band_std"])
-
-        self.price_offset = float(settings["price_offset"])
-        self.size_offset = float(settings["size_offset"])
-        self.volatility_offset = float(settings["volatility_offset"])
+        self.parameters = str(settings["parameters"])
         
-        self.minimum_spread = float(settings["minimum_spread"])
-        self.num_orders = int(settings["number_of_orders"])
-        self.min_order_size = float(settings["min_order_size"])
-        self.max_order_size = float(settings["max_order_size"])
-        self.max_inventory_delta = float(settings["max_inventory_delta"])
-    
