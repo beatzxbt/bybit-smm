e049c6f02dfbc0ef19933b565bb79a980a9b2bc2
beatzxbt <mpatel.vishva@gmail.com>
2024-01-21 02:08:50 +0000

    add: binance ws handlers

---
+++ frameworks/exchange/brrrclient/binance/handlers/bba.py
@@ -0,0 +1,11 @@
+from typing import List, Dict, Union
+
+class BinanceBbaHandler:
+    def __init__(self, market: Dict) -> None:
+        self.market = market
+
+    def process(self, recv: Union[Dict, List, str]) -> Dict:
+        self.market[recv["s"]]["bba"][0, 0] = float(recv["data"]["b"])
+        self.market[recv["s"]]["bba"][0, 1] = float(recv["data"]["B"])
+        self.market[recv["s"]]["bba"][1, 0] = float(recv["data"]["a"])
+        self.market[recv["s"]]["bba"][1, 1] = float(recv["data"]["A"])
