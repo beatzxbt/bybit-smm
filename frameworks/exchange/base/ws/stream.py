fb9288ecc12176ee202a2017d2d39f437047aff3
beatzxbt <mpatel.vishva@gmail.com>
2024-01-21 02:09:22 +0000

    fix: aiohttp ws funcs

--- frameworks/exchange/base/ws/stream.py
+++ frameworks/exchange/base/ws/stream.py
@@ -1,54 +1,57 @@
 import asyncio
 import aiohttp
 import orjson
-from frameworks.sharedstate import SharedState
 from frameworks.tools.logger import Logger
-from typing import Tuple, List, Dict, Optional, Union
-
+from typing import List, Dict, Union
 
 class WebsocketStream:
 
     def __init__(self, logger: Logger) -> None:
         self.logging = logger
-        self.exchange = None
-        self.ws_url = None
-        self.ws_topics = None
-        self.ws_handler_map = None
-
         self.session = aiohttp.ClientSession()
-        self.active_session = False
+        self.connected = False
 
-    async def connect(self, url: str):
-        """Start ws session"""
-        await self.session.ws_connect(url)
-        self.active_session = True
+    async def _connect_(self, url: str):
+        """Start a websocket connection"""
+        try:
+            self.ws = await self.session.ws_connect(url)
+            self.connected = True
+        except Exception as e:
+            self.logging.error(f"Error connecting to {url}: {e}")
+            self.connected = False
+            raise e
 
-    async def send(self, payload: Union[Dict, List]):
+    async def _send_(self, payload: Union[Dict, List]):
         """Send payload through session"""
-        pass
+        if self.connected:
+            await self.ws.send_json(payload)
 
-    async def start(self):
-        if self.active_session:
-            try:
-                while True:
-                    recv = orjson.loads(await self.session.recv())
+    def _handler_map_(self, recv: Union[Dict, List]):
+        """Map a received msg to its respective handler"""
+        raise NotImplementedError("Must be implemented in inherited class!")
 
-                    if "success" not in recv:
-                        handler = self.stream_handler_map.get(recv["stream"])
+    async def start(self, url: str):
+        try:
+            await self._connect_(url)
+            while True:
+                msg = await self.ws.receive()
 
-                        if handler:
-                            handler(recv)
+                if msg.type in [aiohttp.WSMsgType.TEXT, aiohttp.WSMsgType.BINARY]:
+                    recv = orjson.loads(msg.data)
+                    self._handler_map_(recv["stream"])(recv)
 
-            except aiohttp.ClientConnectionError:
-                self.logging.critical(f"Disconnected from websocket feeds, reconnecting...")
-                pass
+                elif msg.type in [aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.ERROR]:
+                    break
 
-            except Exception as e:
-                self.logging.critical(e)
-                raise e
+        except aiohttp.ClientConnectionError:
+            self.logging.error("Disconnected from websocket feeds, reconnecting...")
+            await asyncio.sleep(1)
+            await self.start(url)
 
-        else:
-            self.logging.critical(f"Websocket connection inactive!")
+        except Exception as e:
+            self.logging.critical(f"Exception in websocket stream: {e}")
+            raise
 
-    async def start_feed(self):
-        await self._stream()
+    async def close(self):
+        await self.ws.close()
+        await self.session.close()
