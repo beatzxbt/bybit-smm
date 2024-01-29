23f924b77ed06fa4519fc1389f85a59a9f152ece
beatzxbt <mpatel.vishva@gmail.com>
2024-01-29 22:24:00 +0000

    add: abs & clip

--- frameworks/tools/numba_funcs.py
+++ frameworks/tools/numba_funcs.py
@@ -1,10 +1,25 @@
 import numpy as np
 from numba import njit, float64, uint8
+from numpy.typing import NDArray
 
-@njit(float64[:](float64, float64, uint8))
-def nblinspace(start: float, end: float, n: int) -> np.ndarray:
+@njit(float64[:](float64, float64, uint8), cache=True)
+def nblinspace(start: float, end: float, n: int) -> NDArray:
     return np.linspace(start, end, n)
 
-@njit(float64(float64, uint8))
+@njit(float64(float64, uint8), cache=True)
 def nbround(num: float, digit: int) -> float:
     return np.around(num, digit)
+
+@njit(float64(float64), cache=True)
+def nbabs(val: float) -> float:
+    return np.abs(val)
+    
+@njit(float64(float64, float64, float64), cache=True)
+def nbclip(val: float, min: float, max: float) -> float:
+    if val < min:
+        return min
+    elif val > max:
+        return max
+    else:
+        return val
+
