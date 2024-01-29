0fbbea53f8ae7e8dbf411165ac0f4912567c0f9e
beatzxbt <mpatel.vishva@gmail.com>
2024-01-29 22:23:28 +0000

    add: markPrice stream

--- frameworks/exchange/brrr/binance/handlers/ticker.py
+++ frameworks/exchange/brrr/binance/handlers/ticker.py
@@ -5,8 +5,4 @@ class BinanceTickerHandler:
         self.market = market
 
     def process(self, recv: Dict) -> Dict:
-        self.market[recv["s"]]["markPrice"] = float(recv["data"]["p"])
-        self.market[recv["s"]]["indexPrice"] = float(recv["data"]["i"])
-        self.market[recv["s"]]["fundingRate"] = float(recv["data"]["r"])
-        self.market[recv["s"]]["fundingTimestamp"] = float(recv["data"]["T"])
-        # self.market[recv["s"]]["24hVol"] = None # TODO: Requires mini-ticker stream
+        self.market[recv["s"]]["24hVol"] = float(recv["data"]["v"])
