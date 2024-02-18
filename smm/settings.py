570e8b129bfe662137d89169ad6c0ea80b748543
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-18 17:10:21 +0000

    add: param types

--- smm/settings.py
+++ smm/settings.py
@@ -1,11 +1,13 @@
-from typing import Dict
 from frameworks.config.custom_parameters import CustomParameters
+from typing import Dict, List
 
 
 class SmmParameters(CustomParameters):
+    primary_exchange: List[str]
+    pairs: List[List[str]]
+    parameters: Dict
 
     def process_parameters(self, settings: Dict) -> None:
         self.primary_exchange = str(settings["primary_exchange"])
         self.pairs = list(settings["symbols"])
-        self.parameters = str(settings["parameters"])
-        
+        self.parameters = dict(settings["parameters"])
