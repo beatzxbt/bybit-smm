61f9a0c2351535c536fe6e63cfe29de5a12aa668
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-20 14:08:53 +0000

    add: instrument info

--- src/exchanges/binance/get/client.py
+++ src/exchanges/binance/get/client.py
@@ -1,34 +1,33 @@
-
 from binance.client import Client
+from typing import Dict
 from src.sharedstate import SharedState
 
-
 class BinancePublicGet:
-
-
-    def __init__(self, sharedstate: SharedState) -> None:
-        self.ss = sharedstate
-        self.symbol = self.ss.binance_symbol
+    def __init__(self, ss: SharedState) -> None:
+        self.ss = ss
+        self.symbol: str = self.ss.binance_symbol
         self.client = Client()
 
-
-    async def orderbook_snapshot(self, limit: int) -> dict:
+    async def orderbook(self, limit: int) -> Dict:
         return self.client.get_order_book(
             symbol=self.symbol, 
             limit=limit
         )
 
-
-    async def klines_snapshot(self, limit: int, interval: int) -> dict:
+    async def klines(self, limit: int, interval: int) -> Dict:
         return self.client.get_klines(
             symbol=self.symbol, 
             interval=interval, 
             limit=limit
         )
 
-
-    async def trades_snapshot(self, limit: int) -> dict:
+    async def trades(self, limit: int) -> Dict:
         return self.client.get_recent_trades(
             symbol=self.symbol, 
             limit=limit
+        )
+    
+    async def instrument_info(self) -> Dict:
+        return self.client.get_symbol_info(
+            symbol=self.symbol
         )
--- src/exchanges/bybit/get/public.py
+++ src/exchanges/bybit/get/public.py
@@ -1,32 +1,18 @@
-
 from pybit.unified_trading import HTTP
+from typing import List
 from src.sharedstate import SharedState
 
 
 class BybitPublicClient:
+    category = "linear"
 
+    def __init__(self, ss: SharedState) -> None:
+        self.ss = ss
+        self.key, self.secret = self.ss.api_key, self.ss.api_secret
+        self.session = HTTP(api_key=self.key, api_secret=self.secret)
+        self.symbol: str = self.ss.bybit_symbol
 
-    def __init__(self, sharedstate: SharedState) -> None:
-        self.ss = sharedstate
-        self._session = None
-
-        self.symbol = self.ss.bybit_symbol
-        self.category = "linear"
-
-
-    @property
-    def session(self):
-        if self._session is None:
-            self._session = HTTP(
-                api_key=self.ss.api_key, 
-                api_secret=self.ss.api_secret
-            )
-                
-
-        return self._session
-
-
-    async def klines(self, interval: int, limit: int) -> list:
+    async def klines(self, interval: int, limit: int) -> List:
         return self.session.get_kline(
             category=self.category, 
             symbol=self.symbol, 
@@ -34,9 +20,15 @@ class BybitPublicClient:
             limit=str(limit)
         )
 
-    async def trades(self, limit: int) -> list:
+    async def trades(self, limit: int) -> List:
         return self.session.get_public_trade_history(
             category=self.category, 
             symbol=self.symbol, 
             limit=str(limit)
+        )
+    
+    async def instrument_info(self) -> List:
+        return self.session.get_instruments_info(
+            category=self.category,
+            symbol=self.symbol
         )
