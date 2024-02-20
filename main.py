e923358085fb7702cc259cac8f02df8f25598425
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-20 14:07:58 +0000

    add: uvloop eventloop

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-
 import asyncio
-
+import uvloop
 from src.strategy.core import Strategy
 from src.sharedstate import SharedState
 
-
 async def main():
-
     sharedstate = SharedState()
-    
-    # Run tasks \
     await asyncio.gather(
         asyncio.create_task(sharedstate.refresh_parameters()),
         asyncio.create_task(Strategy(sharedstate).run())
     )
 
-
 if __name__ == "__main__":
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     asyncio.run(main())
-
-
