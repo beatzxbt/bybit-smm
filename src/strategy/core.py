7c5d4583450234f02fdc57e20c6bc0ae155178c9
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-20 14:35:36 +0000

    add: faster ws warmup checker

--- src/strategy/core.py
+++ src/strategy/core.py
@@ -1,25 +1,19 @@
-
 import asyncio
-
-from src.utils.misc import curr_dt
+from typing import Coroutine
+from src.utils.misc import datetime_now as dt_now
 from src.strategy.ws_feeds.bybitmarketdata import BybitMarketData
 from src.strategy.ws_feeds.binancemarketdata import BinanceMarketData
 from src.strategy.ws_feeds.bybitprivatedata import BybitPrivateData
 from src.strategy.marketmaker import MarketMaker
-from src.strategy.diff import Diff
-
+from src.strategy.oms import OMS
 from src.sharedstate import SharedState
 
 
 class DataFeeds:
+    def __init__(self, ss: SharedState) -> None:
+        self.ss = ss
 
-
-    def __init__(self, sharedstate: SharedState) -> None:
-        self.ss = sharedstate
-
-
-    async def start_feeds(self) -> None:
-
+    async def start_feeds(self) -> Coroutine:
         tasks = [
             asyncio.create_task(BybitMarketData(self.ss).start_feed()),
             asyncio.create_task(BybitPrivateData(self.ss).start_feed())
@@ -33,37 +27,32 @@ class DataFeeds:
         await asyncio.gather(*tasks)
 
 
-
 class Strategy:
+    def __init__(self, ss: SharedState) -> None:
+        self.ss = ss
 
-
-    def __init__(self, sharedstate: SharedState) -> None:
-        self.ss = sharedstate
-
-
-    async def logic(self):
-        
-        # Delay to let data feeds warm up
-        print(f"{curr_dt()}: Warming up data feeds...")
-        await asyncio.sleep(10)
-
-        print(f"{curr_dt()}: Starting strategy...")
+    async def _wait_for_ws_confirmation_(self) -> Coroutine:
+        while True: 
+            await asyncio.sleep(1)
+            if self.ss.primary_data_feed == "BINANCE":
+                if self.ss.bybit_ws_connected and self.ss.binance_ws_connected:
+                    break
+            else:
+                if self.ss.bybit_ws_connected: 
+                    break
+                
+    async def primary_loop(self) -> None:
+        print(f"{dt_now()}: Starting data feeds...")
+        await self._wait_for_ws_confirmation_()
+        print(f"{dt_now()}: Starting strategy...")
         
         while True:
-            
-            # Pause for 10ms to let data feeds update
-            await asyncio.sleep(0.01 * 100)
-            
-            # Generate new orders
-            new_orders = MarketMaker(self.ss).generate_quotes()
-            
-            # Diff function will manage new order placements, if any
-            await Diff(self.ss).diff(new_orders)
+            await asyncio.sleep(1)
+            new_orders, spread = MarketMaker(self.ss).generate_quotes(debug=True)
+            await OMS(self.ss).run(new_orders, spread)
 
-
-    async def run(self):
+    async def run(self) -> Coroutine:
         await asyncio.gather(
             asyncio.create_task(DataFeeds(self.ss).start_feeds()),
-            asyncio.create_task(self.logic())
+            asyncio.create_task(self.primary_loop())
         )
-    
