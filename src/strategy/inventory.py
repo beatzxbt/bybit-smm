aa8d7b897aa533057ee86cd58a767fe943d332ac
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-20 14:36:11 +0000

    add: cleaner side checking

--- src/strategy/inventory.py
+++ src/strategy/inventory.py
@@ -1,30 +1,13 @@
 
-
 class Inventory:
-    
-
-    def __init__(self, sharedstate) -> None:
-        self.ss = sharedstate
-
+    def __init__(self, ss) -> None:
+        self.ss = ss
 
     def position_delta(self, side: str, value: float, leverage: int) -> None:
         """
         Calculates the current position delta relative to account size
         """
-        
-        try:
-            if side:
-                acc_max = (self.ss.account_size * leverage) / 2.05
-
-                if side == 'Buy':
-                    self.ss.inventory_delta = value / acc_max
-
-                elif side == 'Sell':
-                    self.ss.inventory_delta = -value / acc_max
-
-        except Exception as e:
-            print(e)
-            print(side, value, leverage)
-
-
-
+        if side:
+            acc_max = (self.ss.account_size * leverage) / 2.05
+            value = value if side == 'Buy' else -value
+            self.ss.inventory_delta = value / acc_max
