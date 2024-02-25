dfeae3c0dd0538d9854a67693c143d8e338fce73
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-25 01:28:25 +0000

    add: docstring

--- src/strategy/features/bba_imbalance.py
+++ src/strategy/features/bba_imbalance.py
@@ -1,6 +1,18 @@
+from numpy.typing import NDArray
 
-def bba_imbalance(bid: float, ask: float) -> float:
+def bba_imbalance(bba: NDArray) -> float:
     """
-    Imbalance between bid and ask quantities
+    Calculates the imbalance between bid and ask quantities.
+
+    Parameters
+    ----------
+    bba : NDArray
+        An array containing the best bid and ask quantities, in the format 
+        [[bid price, bid size], [ask price, ask size]].
+
+    Returns
+    -------
+    float
+        The normalized imbalance between bid and ask quantities. The value ranges from -1 to 1.
     """
-    return ((bid/(ask + bid)) - 0.5) * 2
+    return ((bba[0, 1] / (bba[1, 1] + bba[0, 1])) - 0.5) * 2
--- src/strategy/features/mark_spread.py
+++ src/strategy/features/mark_spread.py
@@ -1,9 +1,31 @@
 import numpy as np
 from numba import njit
 
-@njit
-def log_price_difference(p1: float, p2: float) -> float:
+@njit(cache=True)
+def log_price_difference(follow: float, base: float) -> float:
     """
-    Log difference between price1 relative to price2
+    Calculates the logarithmic price difference between two prices, scaled by 100.
+
+    Parameters
+    ----------
+    follow : float
+        The price you intend on following (eg., price from the most liquid venue).
+    base : float
+        The price you are comparing it to (eg., price from the venue you're trading).
+
+    Returns
+    -------
+    float
+        The logarithmic difference between the two prices, scaled by 100.
+        A positive value indicates that follow price is higher than base price, while a
+        negative value indicates that follow price is lower than base price.
+
+    Examples
+    --------
+    >>> log_price_difference(follow=49000, base=48980)
+    0.04082465866049452
+
+    >>> log_price_difference(follow=48980, base=49000)
+    -0.04082465866049452
     """
-    return np.log(p1/p2) * 100
+    return np.log(base / follow) * 100
