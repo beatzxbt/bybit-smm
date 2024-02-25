8da22bc6cb13a2b14e661c487e60b4a4258ce93d
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-25 01:28:54 +0000

    add: new features + updated weights

--- src/strategy/features/generate.py
+++ src/strategy/features/generate.py
@@ -1,68 +1,106 @@
 import numpy as np
-from src.strategy.features.momentum import momentum_v1
 from src.strategy.features.mark_spread import log_price_difference
 from src.strategy.features.bba_imbalance import bba_imbalance
+from src.strategy.features.ob_imbalance import orderbook_imbalance
+from src.strategy.features.trades_imbalance import trades_imbalance
 from src.sharedstate import SharedState
 
 class Features:
     """
     WARNING: Some features are disabled for Bybit-only streams    
     """
-    depths = np.array([200, 100, 50, 25, 10])
+    _orderbook_depths_ = np.array([10, 25, 50, 100, 200, 500], dtype=np.int64)
+    _trades_window_ = 1000
 
     def __init__(self, ss: SharedState) -> None:
         self.ss = ss
 
     def bybit_mark_wmid_spread(self) -> float:
         return log_price_difference(
-            p1=self.ss.bybit_mark_price, 
-            p2=self.ss.bybit_wmid
+            follow=self.ss.bybit_mark_price, 
+            base=self.ss.bybit_wmid
         )
 
     def binance_bybit_wmid_spread(self) -> float:
         return log_price_difference(
-            p1=self.ss.binance_wmid, 
-            p2=self.ss.bybit_wmid
+            follow=self.ss.binance_wmid, 
+            base=self.ss.bybit_wmid
         )
 
     def bybit_bba_imbalance(self) -> float:
         return bba_imbalance(
-            bid=self.ss.bybit_bba[0][1], 
-            ask=self.ss.bybit_bba[1][1]
+            bba=self.ss.bybit_bba
         )
     
     def binance_bba_imbalance(self) -> float:
         return bba_imbalance(
-            bid=self.ss.binance_bba[0][1], 
-            ask=self.ss.binance_bba[1][1]
+            bba=self.ss.binance_bba
         )
     
     def bybit_wmid_vamp_spread(self) -> float:
         return log_price_difference(
-            p1=self.ss.bybit_vamp, 
-            p2=self.ss.bybit_wmid
+            follow=self.ss.bybit_vamp, 
+            base=self.ss.bybit_wmid
         )
 
     def binance_wmid_vamp_spread(self) -> float:
         return log_price_difference(
-            p1=self.ss.binance_vamp, 
-            p2=self.ss.binance_wmid
+            follow=self.ss.binance_vamp, 
+            base=self.ss.binance_wmid
+        )
+    
+    def bybit_orderbook_imbalance(self) -> float:
+        return orderbook_imbalance(
+            bids=self.ss.bybit_book.bids,
+            asks=self.ss.bybit_book.asks,
+            depths=self._orderbook_depths_
+        )
+
+    def binance_orderbook_imbalance(self) -> float:
+        return orderbook_imbalance(
+            bids=self.ss.binance_book.bids,
+            asks=self.ss.binance_book.asks,
+            depths=self._orderbook_depths_
+        )
+    
+    def bybit_trades_imbalance(self) -> float:
+        return trades_imbalance(
+            trades=self.ss.bybit_trades._unwrap(), 
+            window=self._trades_window_
+        )
+
+    def binance_trades_imbalance(self) -> float:
+        return trades_imbalance(
+            trades=self.ss.binance_trades._unwrap(), 
+            window=self._trades_window_
         )
 
     def generate_skew(self) -> float:
         total_skew = 0
 
         if self.ss.primary_data_feed == "BINANCE":
-            total_skew += self.bybit_mark_wmid_spread() * 0.2
-            total_skew += self.binance_bybit_wmid_spread() * 0.1
-            total_skew += self.bybit_bba_imbalance() * 0.1
-            total_skew += self.binance_bba_imbalance() * 0.2
-            total_skew += self.bybit_wmid_vamp_spread() * 0.2
-            total_skew += self.binance_wmid_vamp_spread() * 0.2
+            # Total weight = 0.4
+            total_skew += self.bybit_bba_imbalance() * 0.025
+            total_skew += self.binance_bba_imbalance() * 0.025
+            total_skew += self.bybit_mark_wmid_spread() * 0.075
+            total_skew += self.binance_bybit_wmid_spread() * 0.075
+            total_skew += self.bybit_wmid_vamp_spread() * 0.075
+            total_skew += self.binance_wmid_vamp_spread() * 0.075
+
+            # Total weight = 0.6
+            total_skew += self.binance_orderbook_imbalance() * 0.2
+            total_skew += self.binance_trades_imbalance() * 0.2
+            total_skew += self.bybit_orderbook_imbalance() * 0.1
+            total_skew += self.bybit_trades_imbalance() * 0.1
 
         else:
-            total_skew += self.bybit_mark_wmid_spread() * 0.4
-            total_skew += self.bybit_bba_imbalance() * 0.2
-            total_skew += self.bybit_wmid_vamp_spread() * 0.4
+            # Total weight = 0.5
+            total_skew += self.bybit_bba_imbalance() * 0.1
+            total_skew += self.bybit_mark_wmid_spread() * 0.15
+            total_skew += self.bybit_wmid_vamp_spread() * 0.15
+
+            # Total weight = 0.5
+            total_skew += self.bybit_orderbook_imbalance() * 0.25
+            total_skew += self.bybit_trades_imbalance() * 0.25
 
         return total_skew
