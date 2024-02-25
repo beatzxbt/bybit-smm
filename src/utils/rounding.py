39b32ad38055c604f60afba1db0af65cc7d3ef5f
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-02-25 01:15:46 +0000

    warn: backtracked changes

--- src/utils/rounding.py
+++ src/utils/rounding.py
@@ -1,10 +1,22 @@
-import numpy as np
-from numba import njit, float64
+from decimal import Decimal
 
-@njit(float64(float64, float64), cache=True)
 def round_step(num: float, step: float) -> float:
     """
     Rounds a float to a given step size
     """
-    p = int(1/step)
-    return np.floor(num*p)/p
+    num = Decimal(str(num))
+    return float(num - num % Decimal(str(step)))
+
+
+### FIX FOR STEP > 1 ###
+
+# import numpy as np
+# from numba import njit, float64
+
+# @njit(float64(float64, float64), cache=True)
+# def round_step(num: float, step: float) -> float:
+#     """
+#     Rounds a float to a given step size
+#     """
+#     p = int(1/step)
+#     return np.floor(num*p)/p
