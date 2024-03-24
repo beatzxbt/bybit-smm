18d2b7f5d75516746d6a03823e5ae3395961a232
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-24 02:07:51 +0000

    fix: removed decimal impl

--- frameworks/tools/rounding.py
+++ frameworks/tools/rounding.py
@@ -1,7 +1,9 @@
-import numpy as np
 from numba import njit, float64
 
 @njit(float64(float64, float64), cache=True)
-def round_step(num: float, step: float) -> float:
-    p = int(1/step)
-    return np.floor(num*p)/p
+def round_num(num: float, step: float) -> float:
+    """
+    Rounds a float to a given step size
+    """
+    p = round(1/step)
+    return round(num*p)/p
