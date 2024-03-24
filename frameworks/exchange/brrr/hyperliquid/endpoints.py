abfa0eaa935ca39f9123a9aad6b579160ac69a12
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-24 02:06:32 +0000

    del: brrr client impl

--- frameworks/exchange/brrr/hyperliquid/endpoints.py
+++
@@ -1,9 +0,0 @@
-
-HyperliquidEndpoints = {
-    "main1": "https://api.hyperliquid.xyz",
-    "pub_ws": "wss://api.hyperliquid.xyz/ws",
-    "priv_ws": "wss://api.hyperliquid.xyz/ws",
-                    
-    "info": ("/info", "POST"),         
-    "exchange": ("/exchange", "POST"),        
-}    
