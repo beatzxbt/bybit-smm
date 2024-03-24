3868c8ec2529fd16f1011ef8127c56143b6c38b0
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-24 02:00:51 +0000

    del: half-stinkbiddor impl

--- stink_biddor/settings.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from frameworks.config.custom_parameters import CustomParameters
-
-class StinkBiddorParameters(CustomParameters):
-    def process_parameters(self, settings: Dict) -> None:
-        self.pair = list(settings["pair"])
-        self.levels = str(settings["levels"])
-        
