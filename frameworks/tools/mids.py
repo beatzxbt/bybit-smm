2db4cccf67e2c8d0a85e4881c344ba883f4b833a
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-25 16:38:07 +0000

    add: level -> volume for depth

--- frameworks/tools/mids.py
+++ frameworks/tools/mids.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numba import njit, float64, int64, bool_
 from numpy.typing import NDArray
+from frameworks.exchange.base.structures.orderbook import Orderbook
 
 @njit(cache=True)
 def exponential_weights(window: int, reverse: bool=True) -> NDArray:
@@ -10,33 +11,34 @@ def exponential_weights(window: int, reverse: bool=True) -> NDArray:
         weights[i] = alpha * (1 - alpha) ** i
     return weights[::-1] if reverse else weights
 
-@njit(cache=True)
 def mid(bba: NDArray) -> float:
     return (bba[1][0] + bba[0][0])/2
 
-@njit(cache=True)
 def wmid(bba: NDArray) -> float:
     imb = bba[0][1] / (bba[0][1] + bba[1][1])
     return bba[1][0] * imb + bba[0][0] * (1 - imb)
 
-@njit(cache=True)
-def vamp(asks: NDArray, bids: NDArray, depth: int) -> float:
-    bids_qty_sum = np.sum(bids[:depth, 1])
-    asks_qty_sum = np.sum(asks[:depth, 1])
-    bid_fair = np.sum(bids[:depth, 0] * (bids[:depth, 1] / bids_qty_sum))
-    ask_fair = np.sum(asks[:depth, 0] * (asks[:depth, 1] / asks_qty_sum))
-    return (ask_fair + bid_fair)/2
+def vamp(orderbook: Orderbook, depth: float) -> float:
+    bid_cum_size = np.cumsum(orderbook.bids[:, 1])
+    bid_levels_till_depth = bid_cum_size[bid_cum_size <= depth].size
+    bid_fair = np.sum(np.prod(orderbook.bids[:bid_levels_till_depth], axis=1) / depth)
+
+    ask_cum_size = np.cumsum(orderbook.asks[:, 1])
+    ask_levels_till_depth = ask_cum_size[ask_cum_size <= depth].size
+    ask_fair = np.sum(np.prod(orderbook.asks[:ask_levels_till_depth], axis=1) / depth)
+
+    return (bid_fair + ask_fair) / 2
 
 @njit(cache=True)
-def weighted_vamp(asks: NDArray, bids: NDArray, depth: int) -> float:
-    weights = exponential_weights(depth) # TODO: Cache this if possible
+def weighted_vamp(orderbook: Orderbook, depth: int) -> float:
+    weights = exponential_weights(depth)
     fair_prices = np.empty(depth, dtype=float64)
-    bids_cum_qty_sum = np.cumsum(bids[:depth, 1])
-    asks_cum_qty_sum = np.cumsum(asks[:depth, 1])
+    bids_cum_qty_sum = np.cumsum(orderbook.bids[:depth, 1])
+    asks_cum_qty_sum = np.cumsum(orderbook.asks[:depth, 1])
 
     for i in range(depth):
-        bid_fair = np.sum(bids[:i+1, 0] * (bids[:i+1, 1] / bids_cum_qty_sum[i]))
-        ask_fair = np.sum(asks[:i+1, 0] * (asks[:i+1, 1] / asks_cum_qty_sum[i]))
+        bid_fair = np.sum(orderbook.bids[:i+1, 0] * (orderbook.bids[:i+1, 1] / bids_cum_qty_sum[i]))
+        ask_fair = np.sum(orderbook.asks[:i+1, 0] * (orderbook.asks[:i+1, 1] / asks_cum_qty_sum[i]))
         fair_prices[i] = (ask_fair + bid_fair) / 2
 
     return np.sum(fair_prices * weights) / np.sum(weights)
