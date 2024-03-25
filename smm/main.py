c042a67acce2c5e1adb18ae4e51c27df301eccba
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-25 16:39:20 +0000

    add: uvloop + keyboard interupt

--- smm/main.py
+++ smm/main.py
@@ -1,30 +1,41 @@
 import asyncio
-from frameworks.sharedstate import SharedState
-from frameworks.tools.logger import Logger
-from smm.strategy.marketmaker import MarketMaker
-from smm.settings import SmmParameters
+import uvloop
+from frameworks.tools.logging import Logger
+from smm.strategy.marketmaker import TradingLogic
+from smm.sharedstate import SmmSharedState
 
 
 async def main():
     try:
-        parameters_directory = ""
-        params = SmmParameters(parameters_directory)
-
-        ss = SharedState()
-        ss.load_markets(params.primary_exchange, params.pairs)
+        backup_logger = Logger
+        ss = SmmSharedState()
+        trading_logic = TradingLogic(ss)
 
         await asyncio.gather(
-            asyncio.to_thread(params.refresh_parameters()),
-            asyncio.to_thread(MarketMaker(ss, params).run())
+            asyncio.create_task(ss.refresh_parameters()),
+            asyncio.create_task(trading_logic.start_loop()),
+            return_exceptions=True
         )
 
+    except KeyboardInterrupt:
+        backup_logger.critical(f"Process manually interupted by user...")
+        return None
+
     except Exception as e:
-        Logger.critical(f"High level exception occurred, shutting down...")
-        # TODO: Implement shut down routine without sharedstate use
+        backup_logger.critical(f"Unexpected exception occurred: {e}")
         raise e
 
     finally:
-        Logger.info("Its late now, goodnight...")
-        
+        backup_logger.critical("Starting shutdown sequence...")
+        pass
+        # """
+        # Implement shutdown sequence here with new sharedstate
+        # It should:
+        #     -> Cancel all open orders
+        #     -> Dump any existing position to market
+        #     -> Cleanup background async tasks 
+        # """
+
 if __name__ == "__main__":
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     asyncio.run(main())
