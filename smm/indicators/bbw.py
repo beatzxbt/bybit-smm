729c348be8dde6a6ed44b7ae59246995f9710387
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-25 16:37:31 +0000

    del: depreciate bbw

--- smm/indicators/bbw.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from numba import njit
-
-@njit
-def bbw(arr_in: np.ndarray, length: int, multiplier: int) -> np.ndarray:
-    """Hyper-fast Bollinger Band Width implementation"""
-    dev = multiplier * np.std(arr_in[-length:])
-    return dev * 2
-    
