2c5962879ec261ca41989f3e634a01db2d21cd13
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-26 20:57:05 +0000

    rename

--- frameworks/tools/numba.py
+++ frameworks/tools/numba.py
@@ -6,13 +6,13 @@ from numpy.typing import NDArray
 def nblinspace(start: float, end: float, n: int) -> NDArray:
     return np.linspace(start, end, n)
 
+@njit(float64[:](float64, float64, uint8), cache=True)
+def nbgeomspace(start: float, end: float, n: int) -> NDArray:
+    return np.geomspace(start, end, n)
+
 @njit(float64(float64, uint8), cache=True)
 def nbround(num: float, digit: int) -> float:
     return np.around(num, digit)
-
-@njit(float64(float64), cache=True)
-def nbabs(val: float) -> float:
-    return np.abs(val)
     
 @njit(float64(float64, float64, float64), cache=True)
 def nbclip(val: float, min: float, max: float) -> float:
@@ -22,4 +22,3 @@ def nbclip(val: float, min: float, max: float) -> float:
         return max
     else:
         return val
-
