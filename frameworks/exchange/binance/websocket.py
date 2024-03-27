6e26b5b7fd9ed104b46e114288984ba0883c8057
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-27 00:05:24 +0000

    add: logging + change for baseclass

--- frameworks/exchange/binance/websocket.py
+++ frameworks/exchange/binance/websocket.py
@@ -4,66 +4,56 @@ from typing import Any, Dict, List, Union
 from frameworks.tools.logging import Logger
 from frameworks.exchange.base.websocket import WebsocketStream
 from frameworks.exchange.base.client import Client
-from frameworks.exchange.brrr.binance.endpoints import BinanceEndpoints
-from frameworks.exchange.brrr.binance.handlers.bba import BinanceBbaHandler
-from frameworks.exchange.brrr.binance.handlers.book import BinanceOrderbookHandler
-from frameworks.exchange.brrr.binance.handlers.trades import BinanceTradesHandler
-from frameworks.exchange.brrr.binance.handlers.ticker import BinanceTickerHandler
-from frameworks.exchange.brrr.binance.handlers.markprice import BinanceMarkPriceHandler
-from frameworks.exchange.brrr.binance.handlers.ohlcv import BinanceOhlcvHandler
-from frameworks.exchange.brrr.binance.handlers.orders import BinanceOrdersHandler
-from frameworks.exchange.brrr.binance.handlers.position import BinancePositionHandler
+from frameworks.exchange.binance.endpoints import BinanceEndpoints
+from frameworks.exchange.binance.handlers.book import BinanceOrderbookHandler
+from frameworks.exchange.binance.handlers.trades import BinanceTradesHandler
+from frameworks.exchange.binance.handlers.markprice import BinanceTickerHandler
+from frameworks.exchange.binance.handlers.ohlcv import BinanceOhlcvHandler
+from frameworks.exchange.binance.handlers.orders import BinanceOrdersHandler
+from frameworks.exchange.binance.handlers.position import BinancePositionHandler
+from frameworks.sharedstate import SharedState
 
 
 class BinanceWebsocket(WebsocketStream):
-    def __init__(self, client: Client, market: Dict[str, Any], private: Union[Dict[str, Any], bool]=False) -> None:        
+    def __init__(self, ss: SharedState, client: Client) -> None:     
+        self.ss = ss   
         self.client = client
-        self.market = market
-        self.private = private 
-        self.logging = Logger
-        super().__init__(isinstance(self.private, Dict))
-        self._set_internal_logger_(self.logging)
+
+        super().__init__()
+        self.set_logger(self.ss.logging)
+
         self.endpoints = BinanceEndpoints
 
         self.public_handler_map = {
-            "bookTicker": BinanceBbaHandler(self._market).process,
-            "depthUpdate": BinanceOrderbookHandler(self._market).process,
-            "trade": BinanceTradesHandler(self._market).process,
-            "kline": BinanceOhlcvHandler(self._market).process,
-            "markPriceUpdate": BinanceMarkPriceHandler(self._market).process,
-            "24hrMiniTicker": BinanceTickerHandler(self._market).process,
+            "depthUpdate": BinanceOrderbookHandler(self.ss).process,
+            "trade": BinanceTradesHandler(self.ss).process,
+            "kline": BinanceOhlcvHandler(self.ss).process,
+            "markPriceUpdate": BinanceTickerHandler(self.ss).process,
         }
+        self.public_handler_map["bookTicker"] = self.public_handler_map["depthUpdate"]
 
         self.private_handler_map = {
-            "ORDER_TRADE_UPDATE": BinanceOrdersHandler(self._private_).process,
-            "ACCOUNT_UPDATE": BinancePositionHandler(self._private_).process,
+            "ORDER_TRADE_UPDATE": BinanceOrdersHandler(self.ss).process,
+            "ACCOUNT_UPDATE": BinancePositionHandler(self.ss).process,
             "listenKeyExpired": None # TODO: Add custom handler here
         }
-    
-    @property
-    def _market_(self) -> Dict:
-        return self.market["binance"]
-
-    @property
-    def _private_(self) -> Dict:
-        return self.private["binance"]
     
     def _gen_public_stream_info_(self, symbols: List[str]) -> str:
         url = self.endpoints["pub_ws"] + "/stream?streams="
         for symbol in symbols:
             url += f"{symbol}@trade/{symbol}@depth@100ms/{symbol}@bookTicker/"
-            url += f"{symbol}@markPrice@1s/{symbol}@kline_1m/{symbol}@miniTicker/"
+            url += f"{symbol}@markPrice@1s/{symbol}@kline_1m"
         return url[:-1]
     
     def _public_stream_handler_(self, recv: Dict) -> None:
         self.public_handler_map[recv["data"]["e"]](recv["data"])
         
-    async def _public_stream_(self, symbols: List[str]):
+    async def public_stream(self, symbols: List[str]):
         try:
             url = self._gen_public_stream_info_(symbols)
-            await self.public_stream(url, self._public_stream_handler_)
+            await self.start_public_ws(url, self._public_stream_handler_)
         except Exception as e:
-            self.logging.error(f"binance public ws: {e}")
+            self.logging.error(f"Binance Public Websocket: {e}")
 
     async def _gen_private_stream_info_(self) -> str:
         listen_key = self.client.listen_key()
@@ -71,18 +61,17 @@ class BinanceWebsocket(WebsocketStream):
         return url[:-1]
     
     def _private_stream_handler_(self, recv: Dict) -> None:
-        data = recv["data"]
-        self.private_handler_map[data["e"]](data)
+        self.private_handler_map[recv["data"]["e"]](recv["data"])
         
-    async def _private_stream_(self):
+    async def private_stream(self):
         try:
             url = self._gen_private_stream_info_()
-            await self.private_stream(url, self._private_stream_handler_)
+            await self.start_private_ws(url, self._private_stream_handler_)
         except Exception as e:
-            self.logging.error(f"binance private ws: {e}")
+            self.logging.error(f"Binance Private Ws: {e}")
     
-    async def start(self, symbols: List[str]) -> None:
-        tasks = [asyncio.create_task(self._public_stream_(symbols))]
-        if isinstance(self.private, Dict):
-            tasks.append(asyncio.create_task(self._private_stream_()))
-        await asyncio.gather(*tasks)
+    async def start(self, symbol: str) -> None:
+        await asyncio.gather(*[
+            asyncio.create_task(self.public_stream(symbol)),
+            asyncio.create_task(self.private_stream())
+        ])
