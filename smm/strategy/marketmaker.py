4468469adec75f68d3bde579a85f3426b3f763d9
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-29 12:29:26 +0000

    add: quote gen switch

--- smm/strategy/marketmaker.py
+++ smm/strategy/marketmaker.py
@@ -1,43 +1,33 @@
 import asyncio
-from frameworks.sharedstate import SharedState
-from smm.settings import SmmParameters
-from smm.strategy.oms import OrderManagementSystem
+from typing import Any 
+from frameworks.datafeed import DataFeed
+from smm.sharedstate import SmmSharedState
+from smm.oms.oms import OrderManagementSystem
 
 
-class MarketMaker:
-
-    def __init__(self, ss: SharedState, params: SmmParameters) -> None:
+class TradingLogic:
+    def __init__(self, ss: SmmSharedState) -> None:
         self.ss = ss
-        self.params = params
-        self.logger = self.ss.logging
-
         self.quote_generator = None
+        self.ws_feed = DataFeed(self.ss)
         self.oms = OrderManagementSystem(self.ss)
 
-    def _strategy_selector_(self) -> None:
-        if self.quote_generator is None:
-            if self.params["quote_generator"] == "simple":
-                from smm.strategy.quote_generators.simple import SimpleQuoteGenerator
-                self.quote_generator = SimpleQuoteGenerator(self.ss, self.params["simple"])
+    def _load_quote_generator_(self) -> Any:
+        if self.params["quote_generator"] == "simple":
+            from smm.quote_generators.simple import SimpleQuoteGenerator
+            return SimpleQuoteGenerator(self.ss)
 
-            elif self.params["quote_generator"] == "basic":
-                from smm.strategy.quote_generators.basic import BasicQuoteGenerator
-                self.quote_generator = BasicQuoteGenerator(self.ss, self.params["basic"])
+        elif self.params["quote_generator"] == "basic":
+            from smm.quote_generators.basic import BasicQuoteGenerator
+            return BasicQuoteGenerator(self.ss)
 
-    async def _loop_(self):
-        self.logger.info("Warming up data feeds...")
+    async def start_loop(self):
+        self.ss.logging.info("Warming up data feeds...")
         await asyncio.sleep(5)
 
-        self.logger.info(f"Starting {self.params['quote_generator']} strategy...")
+        self.ss.logging.info(f"Starting {self.params['quote_generator']} strategy...")
         
         while True:
             await asyncio.sleep(0.01 * 100)
             new_orders = self.quote_generator.generate_quotes()
             await self.oms.update(new_orders)
-
-    async def run(self):
-        await asyncio.gather(
-            await self.params.refresh_parameters(),
-            await self._loop_()
-        )
-    
