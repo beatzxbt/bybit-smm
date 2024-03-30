e3110e930fced452dcfbb93f5e5ccce384e3d021
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-03-30 13:56:17 +0000

    add: jitclass OB + helpers

--- frameworks/exchange/base/structures/orderbook.py
+++ frameworks/exchange/base/structures/orderbook.py
@@ -1,8 +1,26 @@
 import numpy as np
-from typing import Union
+from numba import njit, int64, float64, bool_
+from numba.experimental import jitclass
 from numpy.typing import NDArray
 
+@njit(bool_[:](float64[:], float64[:]), nogil=True)
+def isin(a: NDArray, b: NDArray) -> NDArray:
+    out = np.empty(a.size, dtype=bool_)
+    b = set(b)
 
+    for i in range(a.size):
+        out[i] = True if a[i] in b else False
+            
+    return out
+
+spec = [
+    ('size', int64),
+    ('asks', float64[:, :]),
+    ('bids', float64[:, :]),
+    ('bba', float64[:, :])
+]
+
+@jitclass(spec)
 class Orderbook:
     """
     An orderbook class, maintaining separate arrays for bid and 
@@ -23,11 +41,11 @@ class Orderbook:
     bba : NDArray
         Array to store best bid and ask, each with price and quantity.
     """
-    def __init__(self, size: int):
+    def __init__(self, size: int) -> None:
         self.size = size
-        self.asks = np.empty(shape=(self.size, 2), dtype=np.float64)
-        self.bids = np.empty_like(self.asks)
-        self.bba = np.empty((2, 2), dtype=np.float64)
+        self.asks = np.empty((self.size, 2), dtype=float64)
+        self.bids = np.empty((self.size, 2), dtype=float64)
+        self.bba = np.empty((2, 2), dtype=float64)
 
     def _sort_book_(self) -> NDArray:
         """
@@ -43,17 +61,18 @@ class Orderbook:
         self.bba[0, :] = self.bids[0]
         self.bba[1, :] = self.asks[0]
 
-    def _process_book_(book: NDArray, new_data: NDArray) -> NDArray:
+    def _process_book_(book: NDArray, update: NDArray) -> NDArray:
         """
         Updates the given book with new data. Removes entries with matching 
-        prices in new_data and adds non-zero quantity data from new_data to the book.
+        prices in update, regardless of size, and then adds non-zero quantity 
+        data from update to the book.
 
         Parameters
         ----------
         book : NDArray
             The existing orderbook data (either bids or asks).
 
-        new_data : NDArray
+        update : NDArray
             New order data to be processed into the book.
 
         Returns
@@ -61,12 +80,8 @@ class Orderbook:
         NDArray
             The updated order book data.
         """
-        book = book[~np.isin(
-            book[:, 0],
-            new_data[new_data[:, 1] == 0, 0],
-        )]
-        non_zero_qty_new_data = new_data[new_data[:, 1] != 0]
-        return np.vstack((book, non_zero_qty_new_data))
+        book = book[~isin(book[:, 0], update[:, 0])]
+        return np.vstack((book, update[update[:, 1] != 0]))
 
     def initialize(self, asks: NDArray, bids: NDArray) -> NDArray:
         """
@@ -75,13 +90,13 @@ class Orderbook:
         Parameters
         ----------
         asks : NDArray
-            Initial ask orders data, formatted as [[price, quantity], ...].
+            Initial ask orders data, formatted as [[price, size], ...].
 
         bids : NDArray
-            Initial bid orders data, formatted as [[price, quantity], ...].
+            Initial bid orders data, formatted as [[price, size], ...].
         """
-        self.asks = np.array(asks, dtype=np.float64)
-        self.bids = np.array(bids, np.float64)
+        self.asks[:, :] = asks
+        self.bids[:, :] = bids
         self._sort_book_()
 
     def update_book(self, asks: NDArray, bids: NDArray) -> NDArray:
@@ -91,10 +106,10 @@ class Orderbook:
         Parameters
         ----------
         asks : NDArray
-            Initial ask orders data, formatted as [[price, quantity], ...].
+            Initial ask orders data, formatted as [[price, size], ...].
 
         bids : NDArray
-            Initial bid orders data, formatted as [[price, quantity], ...].
+            Initial bid orders data, formatted as [[price, size], ...].
             
         timestamp : Union[int, float]
             Timestamp of the update.
@@ -102,3 +117,123 @@ class Orderbook:
         self.asks = self._process_book_(self.asks, asks)
         self.bids = self._process_book_(self.bids, bids)
         self._sort_book_()
+
+    def get_mid(self) -> float:
+        """
+        Calculates the mid price of the order book based on the best bid and ask prices.
+
+        Returns
+        -------
+        float
+            The mid price, which is the average of the best bid and best ask prices.
+        """
+        return (self.bba[0, 0] + self.bba[1, 0])/2
+
+    def get_wmid(self) -> float:
+        """
+        Calculates the weighted mid price of the order book, considering the volume imbalance 
+        between the best bid and best ask.
+
+        Returns
+        -------
+        float
+            The weighted mid price, which accounts for the volume imbalance at the top of the book.
+        """
+        imb = self.bba[0, 1] / (self.bba[0, 1] + self.bba[1, 1])
+        return self.bba[0, 0] * imb + self.bba[0, 0] * (1 - imb)
+    
+    def get_vamp(self, depth: float) -> float:
+        """
+        Calculates the volume-weighted average market price (VAMP) up to a specified depth for both bids and asks.
+
+        Parameters
+        ----------
+        depth : float
+            The depth (in terms of volume) up to which the VAMP is calculated.
+
+        Returns
+        -------
+        float
+            The VAMP, representing an average price weighted by order sizes up to the specified depth.
+        """
+        bid_size_weighted_sum = 0.0
+        ask_size_weighted_sum = 0.0
+        bid_cum_size = 0.0
+        ask_cum_size = 0.0
+        
+        # Calculate size-weighted sum for bids
+        for price, size in self.bids:
+            if bid_cum_size + size > depth:
+                remaining_size = depth - bid_cum_size
+                bid_size_weighted_sum += price * remaining_size
+                bid_cum_size += remaining_size
+                break
+
+            bid_size_weighted_sum += price * size
+            bid_cum_size += size
+            
+            if bid_cum_size >= depth:
+                break
+        
+        # Calculate size-weighted sum for asks
+        for price, size in self.asks:
+            if ask_cum_size + size > depth:
+                remaining_size = depth - ask_cum_size
+                ask_size_weighted_sum += price * remaining_size
+                ask_cum_size += remaining_size
+                break
+
+            ask_size_weighted_sum += price * size
+            ask_cum_size += size
+
+            if ask_cum_size >= depth:
+                break
+
+        total_size = bid_cum_size + ask_cum_size
+
+        if total_size == 0:
+            return 0.0
+        
+        return (bid_size_weighted_sum + ask_size_weighted_sum) / total_size
+    
+    def get_spread(self) -> float:
+        """
+        Calculates the current spread of the order book.
+
+        Returns
+        -------
+        float
+            The spread, defined as the difference between the best ask and the best bid prices.
+        """
+        return self.bba[1, 0] - self.bba[0, 0]
+    
+    def get_slippage(self, book: NDArray, size: float) -> float:
+        """
+        Calculates the slippage cost for a hypothetical order of a given size, based on either the bid or ask side of the book.
+
+        Parameters
+        ----------
+        book : NDArray
+            The order book data for the side (bids or asks) being considered.
+            
+        size : float
+            The size of the hypothetical order for which slippage is being calculated.
+
+        Returns
+        -------
+        float
+            The slippage cost, defined as the volume-weighted average deviation from the mid price for the given order size.
+        """
+        mid = self.get_mid()
+        cum_size = 0.
+        slippage = 0.
+
+        for level in range(book.shape[0]):
+            cum_size += book[level, 1]
+            slippage += (np.abs(mid - book[level, 0]) * book[level, 1])
+
+            if cum_size >= size:
+                slippage /= cum_size
+                break
+        
+        return slippage if slippage <= mid else mid
