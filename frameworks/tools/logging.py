389136cb9ab659cb0a17421ef2e6fba7a847d530
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-06 17:54:48 +0100

    add: async + discord support

--- frameworks/tools/logging.py
+++ frameworks/tools/logging.py
@@ -1,3 +1,5 @@
+import orjson
+from aiohttp import ClientSession
 from time import time_ns, strftime
 
 def time_ms() -> int:
@@ -7,20 +9,55 @@ def time_now() -> str:
     return strftime("%Y-%m-%d %H:%M:%S") + f".{(time_ns()//1000) % 1000000:05d}"
 
 class Logger:
-    def success(msg: str) -> None:
-        print(f"{time_now()} | SUCCESS | {msg}")
+    def __init__(self, print_to_console: bool=True, discord_webhook: str="") -> None:
+        self.print_to_console = print_to_console
 
-    def info(msg: str) -> None:
-        print(f"{time_now()} | INFO | {msg}")
+        self.discord_webhook = discord_webhook
+        self.send_to_discord = bool(discord_webhook)
+        self.discord_client = None
+        self.discord_data = {"content": ""}
+        self.discord_headers = {"Content-Type": "application/json"}
+        
+    async def get_discord_client(self) -> ClientSession:
+        if not self.discord_client:
+            self.discord_client = ClientSession()
+            await self.info("Starting up logger...")
 
-    def debug(msg: str) -> None:
-        print(f"{time_now()} | DEBUG | {msg}")
+        return self.discord_client
 
-    def warning(msg: str) -> None:
-        print(f"{time_now()} | WARNING | {msg}")
+    async def close(self) -> None:
+        if self.discord_client:
+            await self.warning("Shutting down logger...")
+            await self.discord_client.close()
+            self.discord_client = None
 
-    def error(msg: str) -> None:
-        print(f"{time_now()} | ERROR | {msg}")
+    async def _message_(self, level: str, msg: str) -> None:
+        if self.print_to_console:
+            print(f"{time_now()} | {level} | {msg}")
+        
+        if self.send_to_discord:
+            self.discord_data["content"] = msg
 
-    def critical(msg: str) -> None:
-        print(f"{time_now()} | CRITICAL | {msg}")
+            await self.get_discord_client().post(
+                url=self.discord_webhook, 
+                data=orjson.dumps(self.discord_data).decode(), 
+                headers=self.discord_headers
+            )
+
+    async def success(self, msg: str) -> None:
+        await self._message_("SUCCESS", msg)
+
+    async def info(self, msg: str) -> None:
+        await self._message_("INFO", msg)
+
+    async def debug(self, msg: str) -> None:
+        await self._message_("DEBUG", msg)
+
+    async def warning(self, msg: str) -> None:
+        await self._message_("WARNING", msg)
+
+    async def error(self, msg: str) -> None:
+        await self._message_("ERROR", msg)
+
+    async def critical(self, msg: str) -> None:
+        await self._message_("CRITICAL", msg)
