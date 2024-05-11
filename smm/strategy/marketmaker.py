2b7735fa9c53fd130480dbad0dc75a143a8b081f
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-11 18:29:12 +0100

    add: improved readability

--- smm/strategy/marketmaker.py
+++ smm/strategy/marketmaker.py
@@ -3,23 +3,32 @@ import asyncio
 from smm.sharedstate import SmmSharedState
 from smm.quote_generators.base import QuoteGenerator
 from smm.oms.oms import OrderManagementSystem
+from smm.features.features import FeatureEngine
 
 
 class TradingLogic:
     def __init__(self, ss: SmmSharedState) -> None:
         self.ss = ss
+        self.feature_engine = FeatureEngine(self.ss)
         self.quote_generator = self.load_quote_generator()
         self.oms = OrderManagementSystem(self.ss)
 
-    def load_quote_generator(self) -> QuoteGenerator:
-        if str(self.params["quote_generator"]).lower() == "plain":
-            from smm.quote_generators.plain import PlainQuoteGenerator
-            return PlainQuoteGenerator(self.ss)
+    async def load_quote_generator(self) -> QuoteGenerator:
+        quote_gen_name = self.params["quote_generator"].lower()
         
-        elif str(self.params["quote_generator"]).lower() == "stinky":
-            from smm.quote_generators.stinky import StinkyQuoteGenerator
-            return StinkyQuoteGenerator(self.ss)
-
+        match quote_gen_name:
+            case "plain":
+                from smm.quote_generators.plain import PlainQuoteGenerator
+                return PlainQuoteGenerator(self.ss)
+            
+            case "stinky":
+                from smm.quote_generators.stinky import StinkyQuoteGenerator
+                return StinkyQuoteGenerator(self.ss)
+            
+            case _:
+                await self.ss.logging.error(f"Invalid quote generator, double check available options!")
+                raise ValueError(f"Invalid quote generator: {quote_gen_name}")
+            
     async def wait_for_ws_warmup(self) -> None:
         """
         Waits for confirmation that the WebSocket connections are 
@@ -50,6 +59,8 @@ class TradingLogic:
         )
         
         while True:
-            await asyncio.sleep(0.01 * 100)
-            new_orders = self.quote_generator.generate_quotes()
+            await asyncio.sleep(1.0)
+            fp_skew = self.feature_engine.generate_skew()
+            vol = self.feature_engine.generate_vol()
+            new_orders = self.quote_generator.generate_orders(fp_skew, vol)
             await self.oms.update(new_orders)
