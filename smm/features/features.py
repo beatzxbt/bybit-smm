5e1f320bc305af6f0b2368f635d79de8a868935d
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-12 21:23:14 +0100

    add: removed duplicate code

--- smm/features/features.py
+++ smm/features/features.py
@@ -6,67 +6,52 @@ from smm.features.trades_diff import trades_diffs
 from smm.features.trades_imbalance import trades_imbalance
 from smm.features.orderbook_imbalance import orderbook_imbalance
 
+
 class FeatureEngine:
     def __init__(self, ss: SmmSharedState) -> None:
         self.ss = ss
 
         self.fair_price_pred = {
-            "wmid":         0.10,
-            "tight_vamp":   0.10,
-            "mid_vamp":     0.15,
-            "deep_vamp":    0.15,
-            "book_imb":     0.25,
-            "trades_imb":   0.25
+            "wmid": 0.10,
+            "tight_vamp": 0.10,
+            "mid_vamp": 0.15,
+            "deep_vamp": 0.15,
+            "book_imb": 0.25,
+            "trades_imb": 0.25,
         }
 
-        self.volatility_pred = {
-            "trades_diffs": 1.0
-        }
-        
+        self.volatility_pred = {"trades_diffs": 1.0}
+
     def wmid_imbalance(self) -> float:
-        return (self.ss.orderbook.get_wmid() / self.ss.orderbook.get_mid())
+        return self.ss.orderbook.get_wmid() / self.ss.orderbook.get_mid()
 
-    def tight_vamp_imbalance(self) -> float:
-        dollars_to_size = 25_000 / self.ss.orderbook.get_mid()
-        return (self.ss.orderbook.get_vamp(dollars_to_size) / self.ss.orderbook.get_mid())
+    def vamp_imbalance(self, depth: int) -> float:
+        dollars_to_size = depth / self.ss.orderbook.get_mid()
+        return self.ss.orderbook.get_vamp(dollars_to_size) / self.ss.orderbook.get_mid()
 
-    def mid_vamp_imbalance(self) -> float:
-        dollars_to_size = 75_000 / self.ss.orderbook.get_mid()
-        return (self.ss.orderbook.get_vamp(dollars_to_size) / self.ss.orderbook.get_mid())
-    
-    def deep_vamp_imbalance(self) -> float:
-        dollars_to_size = 200_000 / self.ss.orderbook.get_mid()
-        return (self.ss.orderbook.get_vamp(dollars_to_size) / self.ss.orderbook.get_mid())
-    
     def orderbook_imbalance(self) -> float:
         return orderbook_imbalance(
             bids=self.ss.orderbook.bids,
             asks=self.ss.orderbook.asks,
-            depths=np.array([10.0, 25.0, 50.0, 100.0, 250.0])
+            depths=np.array([10.0, 25.0, 50.0, 100.0, 250.0]),
         )
-    
+
     def trades_imbalance(self) -> float:
-        return trades_imbalance(
-            trades=self.ss.trades,
-            window=100
-        )
-    
+        return trades_imbalance(trades=self.ss.trades, window=100)
+
     def trades_differences(self) -> float:
-        return trades_diffs(
-            trades=self.ss.trades,
-            lookback=100
-        )
+        return trades_diffs(trades=self.ss.trades, lookback=100)
 
     def generate_skew(self) -> float:
         skew = 0.0
         skew += self.wmid_imbalance() * self.weights["wmid"]
-        skew += self.tight_vamp_imbalance() * self.weights["tight_vamp"]
-        skew += self.mid_vamp_imbalance() * self.weights["mid_vamp"]
-        skew += self.deep_vamp_imbalance() * self.weights["deep_vamp"]
+        skew += self.vamp_imbalance(depth=25000) * self.weights["tight_vamp"]
+        skew += self.vamp_imbalance(depth=75000) * self.weights["mid_vamp"]
+        skew += self.vamp_imbalance(depth=200000) * self.weights["deep_vamp"]
         skew += self.orderbook_imbalance() * self.weights["book_imb"]
         skew += self.trades_imbalance() * self.weights["trades_imb"]
         return skew
-    
+
     def generate_vol(self) -> float:
         vol = 0.0
         vol += self.trades_differences() * self.volatility_pred["trades_diffs"]
