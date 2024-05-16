eb454dfa16d9675bd13291f6894c13826d996e60
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-16 23:57:00 +0100

    add: load exch on init

--- smm/sharedstate.py
+++ smm/sharedstate.py
@@ -5,16 +5,25 @@ from frameworks.sharedstate import SharedState
 
 
 class SmmSharedState(SharedState):
-    def set_parameters_path(self) -> str:
-        return os.path.dirname(os.path.realpath(__file__)) + "/../parameters.yaml"  
-    
-    def process_parameters(self, settings: Dict, reload: bool) -> None:
-        if not reload:
-            self.exchange = settings["exchange"]
-            self.symbol = settings["symbol"]
-            self.quote_generator = settings["quote_generator"]
-
-        self.parameters = settings["parameters"]
+    required_keys = {"max_position", "total_orders"}
 
     def __init__(self) -> None:
         super().__init__()
+
+    def set_parameters_path(self) -> str:
+        return os.path.dirname(os.path.realpath(__file__)) + "/parameters.yaml"
+
+    def process_parameters(self, settings: Dict, reload: bool) -> None:
+        self.parameters: Dict = settings["parameters"]
+
+        if not reload:
+            self.load_exchange(settings["exchange"])
+            self.symbol: str = settings["symbol"]
+            self.quote_generator: str = settings["quote_generator"]
+
+        if self.required_keys <= self.parameters.keys():
+            for key in self.required_keys:
+                if key in self.parameters:
+                    continue
+
+                self.logging.error(f"Missing {key} in params, this is required!")
