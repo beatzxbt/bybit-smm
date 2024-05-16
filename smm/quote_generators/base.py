ddc7506c464f45102c5cba7eef50f9ce180945f4
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-17 00:00:09 +0100

    add: finish plain + seperate breakdowns

--- smm/quote_generators/base.py
+++ smm/quote_generators/base.py
@@ -15,50 +15,54 @@ class QuoteGenerator(ABC):
     @property
     def mid(self) -> float:
         return self.ss.orderbook.get_mid()
-    
+
     @property
     def wmid_price(self) -> float:
         return self.ss.orderbook.get_wmid()
-    
+
     @property
     def live_best_bid(self) -> NDArray:
         return self.ss.orderbook.bids[0]
-    
+
     @property
     def live_best_ask(self) -> NDArray:
         return self.ss.orderbook.asks[0]
-    
+
     @property
     def inventory_delta(self) -> float:
-        size_to_dollar = (self.ss.current_position["size"] * self.ss.current_position["price"])
+        size_to_dollar = (
+            self.ss.current_position["size"] * self.ss.current_position["price"]
+        )
         return size_to_dollar / self.ss.parameters["max_position"]
-    
+
     @property
     def total_orders(self) -> int:
         return self.ss.parameters["total_orders"]
-    
+
     @property
     def max_position(self) -> float:
         """Converts USD position in params to quote size"""
         return self.ss.parameters["max_position"] / self.ss.orderbook.get_mid()
-    
+
     def bps_to_decimal(self, bps: float) -> float:
         return bps / 10000
-    
+
     def bps_offset_to_decimal(self, bps: float) -> float:
         return self.mid + (self.mid * self.bps_to_decimal(bps))
-    
+
     def offset_to_decimal(self, offset: float) -> float:
         return self.mid + (self.mid * offset)
-        
-    def generate_single_quote(self, side: float, orderType: float, price: float, size: float) -> Dict:
+
+    def generate_single_quote(
+        self, side: float, orderType: float, price: float, size: float
+    ) -> Dict:
         return {
             "side": side,
             "orderType": orderType,
             "price": price,
             "size": size,
         }
-    
+
     @abstractmethod
     def generate_orders(self, fp_skew: float, vol: float) -> List[Tuple]:
         """
--- smm/quote_generators/plain.py
+++ smm/quote_generators/plain.py
@@ -11,11 +11,7 @@ from smm.sharedstate import SmmSharedState
 
 class PlainQuoteGenerator(QuoteGenerator):
     """
-    This strategy uses an aggressiveness value to determine how powerful the price prediction 
-    features are in influencing the quote's skew. 
-
-    When closer to the midprice, the best bid/ask tends to be the most filled quotes
-    and such their prices/sizes are very inflencial in resultant PnL. In this case, 
+    This strategy's breakdown can be found in quote_generators.md
     """
     def __init__(self, ss: SmmSharedState) -> None:
         self.ss = ss
@@ -35,7 +31,7 @@ class PlainQuoteGenerator(QuoteGenerator):
         float
             The corrected skew value.
         """
-        corrective_amount = self.inventory_delta ** 2.0
+        corrective_amount = self.inventory_delta**2.0
         skew += corrective_amount if self.inventory_delta < 0.0 else -corrective_amount
         return skew
 
@@ -58,13 +54,15 @@ class PlainQuoteGenerator(QuoteGenerator):
             return min_spread
         else:
             return nbclip(spread, spread, min_spread * 5)
-    
-    def prepare_orders(self, bid_prices: Array, bid_sizes: Array, ask_prices: Array, ask_sizes: Array) -> List[Tuple]:
-        """
-        Prepare bid and ask orders based on given prices and sizes. 
 
-        Ordering is done 1-bid-1-ask for greater priority to inner (closer to mid) orders 
-        and decreasing priority going outward (away from mid). 
+    def prepare_orders(
+        self, bid_prices: Array, bid_sizes: Array, ask_prices: Array, ask_sizes: Array
+    ) -> List[Tuple]:
+        """
+        Prepare bid and ask orders based on given prices and sizes.
+
+        Ordering is done 1-bid-1-ask for greater priority to inner (closer to mid) orders
+        and decreasing priority going outward (away from mid).
 
         Parameters
         ----------
@@ -87,34 +85,40 @@ class PlainQuoteGenerator(QuoteGenerator):
         """
         orders = []
 
-        for (bid_price, bid_size, ask_price, ask_size) in zip(bid_prices, bid_sizes, ask_prices, ask_sizes):
-            orders.append(self.generate_single_quote(
-                side=0.0,
-                orderType=0.0,
-                price=round_floor(num=bid_price, step_size=self.tick_size),
-                size=round_ceil(num=bid_size, step_size=self.lot_size) 
-            ))
+        for bid_price, bid_size, ask_price, ask_size in zip(
+            bid_prices, bid_sizes, ask_prices, ask_sizes
+        ):
+            orders.append(
+                self.generate_single_quote(
+                    side=0.0,
+                    orderType=0.0,
+                    price=round_floor(num=bid_price, step_size=self.tick_size),
+                    size=round_ceil(num=bid_size, step_size=self.lot_size)
+                )
+            )
 
-            orders.append(self.generate_single_quote(
-                side=1.0,
-                orderType=0.0,
-                price=round_ceil(num=ask_price, step_size=self.tick_size),
-                size=round_ceil(num=ask_size, step_size=self.lot_size)
-            ))
+            orders.append(
+                self.generate_single_quote(
+                    side=1.0,
+                    orderType=0.0,
+                    price=round_ceil(num=ask_price, step_size=self.tick_size),
+                    size=round_ceil(num=ask_size, step_size=self.lot_size)
+                )
+            )
 
         return orders
-    
+
     def generate_positive_skew_quotes(self, skew: float, spread: float) -> List[Tuple]:
         """
-        Generate positively skewed bid/ask quotes, with the intention to fill 
+        Generate positively skewed bid/ask quotes, with the intention to fill
         more on the bid side (buy more) than the ask side (sell less). A larger strategy
         breakdown can be found in the README.md, or at the top of this class.
-        
+
         Parameters
         ----------
         skew : float
             A value between -1 <-> 1 predicting the future price over some time horizon.
-            
+
         spread : float
             A value in dollars of minimum price deviation over some time horizon.
 
@@ -124,50 +128,48 @@ class PlainQuoteGenerator(QuoteGenerator):
             A list of single quotes.
         """
         half_spread = spread / 2
-        aggressiveness = self.params["aggressiveness"] * (skew ** 0.5)
+        aggressiveness = self.params["aggressiveness"] * (skew**0.5)
 
         best_bid_price = self.mid - (half_spread * (1.0 - aggressiveness))
         best_ask_price = best_bid_price + spread
 
         bid_prices = nbgeomspace(
             start=best_bid_price,
-            end=best_bid_price - (spread ** 1.5),
-            n=self.total_orders//2
+            end=best_bid_price - (spread**1.5),
+            n=self.total_orders // 2,
         )
 
         ask_prices = nbgeomspace(
             start=best_ask_price,
-            end=best_ask_price + (spread ** 1.5),
-            n=self.total_orders//2
+            end=best_ask_price + (spread**1.5),
+            n=self.total_orders // 2,
         )
 
-        clipped_r = 0.5 + nbclip(skew, 0.0, 0.5)   # NOTE: Geometric ratio cant exceed 1.0
+        clipped_r = 0.5 + nbclip(skew, 0.0, 0.5)  # NOTE: Geometric ratio cant exceed 1.0
 
         bid_sizes = self.max_position * generate_geometric_weights(
-            num=self.total_orders//2,
-            r=clipped_r,
-            reverse=True
+            num=self.total_orders // 2, r=clipped_r, reverse=True
         )
-        
+
         ask_sizes = self.max_position * generate_geometric_weights(
-            num=self.total_orders//2,
+            num=self.total_orders // 2,
             r=0.5 + (clipped_r ** (2 + aggressiveness)),
-            reverse=True
+            reverse=True,
         )
 
         return self.prepare_orders(bid_prices, bid_sizes, ask_prices, ask_sizes)
 
     def generate_negative_skew_quotes(self, skew: float, spread: float) -> List:
         """
-        Generate positively skewed bid/ask quotes, with the intention to fill 
+        Generate positively skewed bid/ask quotes, with the intention to fill
         more on the bid side (buy more) than the ask side (sell less). A larger strategy
         breakdown can be found in the README.md, or at the top of this class.
-        
+
         Parameters
         ----------
         skew : float
             A value between -1 <-> 1 predicting the future price over some time horizon.
-            
+
         spread : float
             A value in dollars of minimum price deviation over some time horizon.
 
@@ -177,35 +179,35 @@ class PlainQuoteGenerator(QuoteGenerator):
             A list of single quotes generated from self.generate_single_quote()
         """
         half_spread = spread / 2
-        aggressiveness = self.params["aggressiveness"] * (skew ** 0.5)
+        aggressiveness = self.params["aggressiveness"] * (skew**0.5)
 
         best_ask_price = self.mid + (half_spread * (1.0 - aggressiveness))
         best_bid_price = best_ask_price - spread
 
         bid_prices = nbgeomspace(
             start=best_bid_price,
-            end=best_bid_price - (spread ** 1.5),
-            n=self.total_orders//2
+            end=best_bid_price - (spread**1.5),
+            n=self.total_orders // 2,
         )
 
         ask_prices = nbgeomspace(
             start=best_ask_price,
-            end=best_ask_price + (spread ** 1.5),
-            n=self.total_orders//2
+            end=best_ask_price + (spread**1.5),
+            n=self.total_orders // 2,
         )
 
-        clipped_r = 0.5 + nbclip(skew, 0.0, 0.5)   # NOTE: Geometric ratio cant exceed 1.0
+        clipped_r = 0.5 + nbclip(
+            skew, 0.0, 0.5
+        )  # NOTE: Geometric ratio cant exceed 1.0
 
         bid_sizes = self.max_position * generate_geometric_weights(
-            num=self.total_orders//2,
+            num=self.total_orders // 2,
             r=0.5 + (clipped_r ** (2 + aggressiveness)),
-            reverse=True
+            reverse=True,
         )
-        
+
         ask_sizes = self.max_position * generate_geometric_weights(
-            num=self.total_orders//2,
-            r=clipped_r,
-            reverse=True
+            num=self.total_orders // 2, r=clipped_r, reverse=True
         )
 
         return self.prepare_orders(bid_prices, bid_sizes, ask_prices, ask_sizes)
