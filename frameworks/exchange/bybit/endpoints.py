f891c207115d74149b07d3f533d878966f22d75f
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-21 16:08:12 +0100

    add: ticker + spelling fix

--- frameworks/exchange/bybit/endpoints.py
+++ frameworks/exchange/bybit/endpoints.py
@@ -6,10 +6,11 @@ BybitEndpoints = {
 
     "ping": ("/v5/market/time", "GET"),                         
     "exchangeInfo": ("/fapi/v1/exchangeInfo", "GET"),      
-    "instrumentInfo": ("/v5/market/instruments-info", "GET"), # NOTE: tick/lot sizes in filters (query per symbol)
+    "instrumentInfo": ("/v5/market/instruments-info", "GET"),
     "orderbook": ("/v5/market/orderbook", "GET"),                   
     "trades": ("/v5/market/recent-trade", "GET"),                     
-    "ohlcv": ("/v5/market/kline", "GET"),                      
+    "ohlcv": ("/v5/market/kline", "GET"),                       
+    "ticker": ("/v5/market/tickers", "GET"),                            
     "allOpenOrders": ("/v5/order/realtime", "GET"),          
     "accountInfo": ("/v5/account/wallet-balance", "GET"),               
     "positionInfo": ("/v5/position/list", "GET"),          
