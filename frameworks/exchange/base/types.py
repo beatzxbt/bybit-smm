c99800a1c82e1ac7fc2f0f22c54b3986e762bd6f
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-21 16:13:14 +0100

    add: depreciate enums for dict conversion

--- frameworks/exchange/base/types.py
+++ frameworks/exchange/base/types.py
@@ -1,64 +1,14 @@
-from enum import Enum
+class BaseStrNumConverter:
+    DEFAULT_UNKNOWN_STR = "UNKNOWN"
+    DEFAULT_UNKNOWN_NUM = -1.0
 
-class BaseOrderTypes:
-    class OrderType(Enum):
-        """ 
-        Placeholder enum class to be overwritten in subclass
-
-        There must be at least market/limit type placeholders
-
-        As a default, here are the following formats for implementations:
-        
-        ------------------------------------------------------------
-        LIMIT = 0
-        MARKET = 1
-        STOP_LOSS = 2
-        STOP_LOSS_LIMIT = 3
-        TAKE_PROFIT = 4
-        TAKE_PROFIT_LIMIT = 5
-        ------------------------------------------------------------
-        """
-        pass
+    num_to_str = {}
+    str_to_num = {}
     
     @classmethod
-    def to_type(cls, value: int) -> str:
-        try:
-            return cls.OrderType(value).name
-        except ValueError:
-            raise ValueError(f"Unknown order type value: {value}")
+    def to_str(cls, value: float) -> str:
+        return cls.num_to_str.get(value, cls.DEFAULT_UNKNOWN_STR)
 
     @classmethod
-    def to_int(cls, name: str) -> int:
-        try:
-            return cls.OrderType[name].value
-        except KeyError:
-            raise ValueError(f"Unknown order type name: {name}")
-        
-
-class BaseOrderSides:
-    class OrderSide(Enum):
-        """ 
-        Placeholder enum class to be overwritten in subclass
-
-        As a default, here are the following formats for implementations:
-        
-        ------------------------------------------------------------
-        BUY = 0
-        SELL = 1
-        ------------------------------------------------------------
-        """
-        pass
-    
-    @classmethod
-    def to_side(cls, value: int) -> str:
-        try:
-            return cls.OrderSide(value).name 
-        except ValueError:
-            raise ValueError(f"Unknown order side value: {value}")
-
-    @classmethod
-    def to_int(cls, name: str) -> int:
-        try:
-            return cls.OrderSide[name].value
-        except KeyError:
-            raise ValueError(f"Unknown order type name: {name}")
+    def to_num(cls, name: str) -> float:
+        return cls.str_to_num.get(name, cls.DEFAULT_UNKNOWN_NUM)
