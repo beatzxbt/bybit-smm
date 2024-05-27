e750c69610acc06be04d94aa1b64e554e5c233fd
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-27 21:23:01 +0100

    add: rename class

--- frameworks/exchange/binance/types.py
+++ frameworks/exchange/binance/types.py
@@ -1,10 +1,10 @@
-from frameworks.exchange.base.types import BaseStrNumConverter
+from frameworks.exchange.base.types import StrNumConverter
 
-class BinanceOrderSides(BaseStrNumConverter):
+class BinanceOrderSides(StrNumConverter):
     str_to_num = {"BUY": 0.0, "SELL": 1.0}
     num_to_str = {v: k for k, v in str_to_num.items()}
 
 
-class BinanceOrderTypes(BaseStrNumConverter):
+class BinanceOrderTypes(StrNumConverter):
     str_to_num = {"LIMIT": 0.0, "MARKET": 1.0, "STOP": 2.0, "TAKE_PROFIT": 3.0, "LIQUIDATION": 9.0}
     num_to_str = {v: k for k, v in str_to_num.items()}
