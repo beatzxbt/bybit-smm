343b7b2d40b184c3ffef575c86d1c09f80250941
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-27 21:30:20 +0100

    add: logging + adapt to new ss

--- smm/strategy/marketmaker.py
+++ smm/strategy/marketmaker.py
@@ -10,21 +10,19 @@ class TradingLogic:
     def __init__(self, ss: SmmSharedState) -> None:
         self.ss = ss
         self.feature_engine = FeatureEngine(self.ss)
-        self.quote_generator = self.load_quote_generator()
         self.oms = OrderManagementSystem(self.ss)
 
     async def load_quote_generator(self) -> QuoteGenerator:
         quote_gen_name = self.ss.quote_generator.lower()
+        await self.ss.logging.info(f"Attempting to load quote generator: {quote_gen_name}")
 
         match quote_gen_name:
             case "plain":
                 from smm.quote_generators.plain import PlainQuoteGenerator
-
                 return PlainQuoteGenerator(self.ss)
 
             case "stinky":
                 from smm.quote_generators.stinky import StinkyQuoteGenerator
-
                 return StinkyQuoteGenerator(self.ss)
 
             case _:
@@ -41,25 +39,29 @@ class TradingLogic:
         while True:
             await asyncio.sleep(1.0)
 
-            if self.ss.trades.shape[0] < 100:
+            if len(self.ss.data["trades"]) < 100:
                 continue
 
-            if self.ss.ohlcv.shape[0] < 100:
+            if len(self.ss.data["ohlcv"]) < 100:
                 continue
 
-            if not bool(self.ss.data["ticker"]):
+            if len(self.ss.data["ticker"].items()) == 0:
                 continue
 
-            if self.ss.data["tick_size"] == 0.0 or self.ss.data["lot_size"] == 0.0:
-                continue
+            # if self.ss.data["orderbook"].bids.:
+            #     continue
 
+            if (self.ss.data["tick_size"], self.ss.data["lot_size"]) == (0.0, 0.0):
+                continue
+            
             break
 
     async def start_loop(self) -> None:
         await self.ss.logging.info("Warming up data feeds...")
         await self.wait_for_ws_warmup()
-        await self.ss.logging.success(
-            f"Starting {self.ss.quote_generator.lower()} strategy on {self.ss.symbol}"
+        self.quote_generator = await self.load_quote_generator()
+        await self.ss.logging.info(
+            f"Starting '{self.ss.quote_generator.lower()}' strategy on {self.ss.symbol}..."
         )
 
         while True:
