4935ac9c183058686614300e866ac416d82e18f8
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-05-27 21:25:09 +0100

    fix: custom numba np.diff

--- frameworks/tools/numba.py
+++ frameworks/tools/numba.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import njit
+from numba.types import Array, bool_
 from numpy.typing import NDArray
 
 
@@ -26,3 +27,63 @@ def nbclip(val: float, min: float, max: float) -> float:
         return max
     else:
         return val
+    
+
+@njit(fastmath=True)
+def nbdiff_1d(a: Array, n: int=1) -> Array:
+    """
+    Compute the n-th discrete difference along the given axis for 1D arrays.
+
+    Parameters
+    ----------
+    a : Array
+        Input array of any shape.
+
+    n : int, optional
+        The number of times values are differenced. If zero, the input is returned as-is.
+        Default is 1.
+
+    Returns
+    -------
+    Array
+        The n-th differences. The shape of the output is the same as `a` except along the 
+        given axis. The length of the shape along that axis is `max(a.shape[axis] - n, 0)`.
+    """
+    if n == 0:
+        return a.copy()
+    
+    if n < 0:
+        raise ValueError("diff(): order must be non-negative")
+    
+    a_size = a.size
+    out_size = max(a_size - n, 0)
+    out = np.empty(out_size, dtype=a.dtype)
+    
+    if out_size == 0:
+        return out
+    
+    work = np.empty_like(a)
+    
+    # First iteration: diff a into work
+    for i in range(a_size - 1):
+        work[i] = a[i + 1] - a[i]
+    
+    # Other iterations: diff work into itself
+    for niter in range(1, n):
+        for i in range(a_size - niter - 1):
+            work[i] = work[i + 1] - work[i]
+    
+    # Copy final diff into out
+    out[:] = work[:out_size]
+
+    return out
+
+@njit(fastmath=True)
+def nbisin(a: Array, b: Array) -> Array:
+    out = np.empty(a.size, dtype=bool_)
+    b = set(b)
+
+    for i in range(a.size):
+        out[i] = True if a[i] in b else False
+
+    return out
--- smm/features/trades_diff.py
+++ smm/features/trades_diff.py
@@ -2,16 +2,19 @@ import numpy as np
 from numba import njit
 from numba.types import Array
 
+from frameworks.tools.numba import nbdiff_1d
+
 
 @njit(["float64(float64[:, :], int64)"], error_model="numpy", fastmath=True)
 def trades_diffs(trades: Array, lookback: int = 100) -> float:
     """
     Computes the sum of the absolute differences of trade prices over a specified lookback period.
 
-    Steps:
-        -> Filter out prices from the trades array.
-        -> Compute absolute differences element-by-element.
-        -> Sum those differences then return.
+    Steps
+    -----
+    1. Filter out prices from the trades array.
+    2. Compute absolute differences element-by-element.
+    3. Sum those differences then return.
 
     Parameters
     ----------
@@ -27,5 +30,5 @@ def trades_diffs(trades: Array, lookback: int = 100) -> float:
         The sum of the absolute differences in trade prices for the specified number of recent trades.
     """
     prices = trades[-lookback:, 2]
-    abs_diff = np.abs(np.diff(prices))
+    abs_diff = np.abs(nbdiff_1d(prices))
     return abs_diff.sum()
